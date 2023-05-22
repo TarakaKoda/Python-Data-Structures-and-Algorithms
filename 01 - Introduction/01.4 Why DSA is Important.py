30cdb4552c391d4466b7fdec69ffc4218288045d
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 22:07:35 +0530

    Why Data Structures and Algorithms are important

---
+++ 01 - Introduction/01.4 Why DSA is Important.py
@@ -0,0 +1,12 @@
+class introduction:
+    '''
+    Why data structures and algorithms are important?
+        Data Processing
+        input Data -> Processing -> Output
+        Data might be of any type, So data structures organize the data and algorithms processes them.
+        Problem-solving skills
+        Fundamental concepts of programming in limited time
+    '''
+
+if __name__ == "__main__":
+    print(introduction().__doc__)
