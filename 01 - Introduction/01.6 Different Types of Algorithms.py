e35012b2101190e00b7aa26f03e7065f9edd8e76
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 22:08:44 +0530

    Different Types of Algorithms

---
+++ 01 - Introduction/01.6 Different Types of Algorithms.py
@@ -0,0 +1,13 @@
+class introduction:
+    '''
+     Different types of Algorithms:
+        - Simple recursive algorithms
+        - Divide and conquer algorithms
+        - Dynamic programming algorithms
+        - Greedy algorithms
+        - Brute Force algorithms
+        - Randomized algorithms
+    '''
+
+if __name__ == "__main__":
+    print(introduction().__doc__)
