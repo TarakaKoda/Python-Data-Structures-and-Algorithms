625747c77319677f0a3e55213717c622448c75f2
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 22:06:36 +0530

    Introduction to Algorithms

---
+++ 01 - Introduction/01.3 What are Algorithms.py
@@ -0,0 +1,11 @@
+class introduction:
+    '''
+    A set of rules for a computer program to accomplish a task
+        input Data -> Calculation -> Stop when answer found
+        What makes a good algorithm?
+        1. Correctness
+        2. Efficiency
+    '''
+
+if __name__ == "__main__":
+    print(introduction().__doc__)
