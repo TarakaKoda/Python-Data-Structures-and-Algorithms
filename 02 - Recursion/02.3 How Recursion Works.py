349de727a72962a10dc8a716841d97ad73c1d55b
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 23:13:01 +0530

    How recursion works

---
+++ 02 - Recursion/02.3 How Recursion Works.py
@@ -0,0 +1,14 @@
+class recursion:
+    '''
+     1. A method calls itself
+     2. Exit from infinite loop
+
+      def recursionMethod(parameters):
+        if exit from condition satisfied:
+            return some value
+        else:
+            recursionMethod(modified parameters)
+    '''
+
+if __name__ == "__main__":
+    print(recursion().__doc__)
