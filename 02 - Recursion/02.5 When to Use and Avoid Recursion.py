6210e2b7f2ff00a96da0a383ee87acdf8fb95a59
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 23:14:00 +0530

    When to use/avoid recursion

---
+++ 02 - Recursion/02.5 When to Use and Avoid Recursion.py
@@ -0,0 +1,16 @@
+class recursion:
+    '''
+    when to use it?
+        - when we can easily break down a problem into similar sub-problems.
+        - when we are fine with extra overhead (both time and space) that comes with it.
+        - when we need a quick working solution instead of efficient one
+        - when traverse a tee
+        - when we use memoization in recursion
+        When we can avoid recursion?
+        - If time and space complexity matters for us
+        - Recursion uses more memory. If we use embedded memory. For example an application that takes more memory in the phone is not efficient.
+        - Recursion can be slow
+    '''
+
+if __name__ == "__main__":
+    print(recursion().__doc__)
