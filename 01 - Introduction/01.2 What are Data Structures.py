8db247c7cf2958b20060b53bc34fd2b8808d2f25
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 22:06:07 +0530

    Introduction to Data Structures

---
+++ 01 - Introduction/01.2 What are Data Structures.py
@@ -0,0 +1,7 @@
+class introduction:
+    '''
+    Data Structures are used to organize and store data efficiently. They provide a way to manage and manipulate collections of data elements.
+    '''
+
+if __name__ == "__main__":
+    print(introduction().__doc__)
