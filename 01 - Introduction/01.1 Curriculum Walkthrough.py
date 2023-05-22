466ad85e67daab7068226ecc19a5ca85d299ce6e
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 22:05:30 +0530

    Add curriculum walkthrough for Data Structure in Python

---
+++ 01 - Introduction/01.1 Curriculum Walkthrough.py
@@ -0,0 +1,36 @@
+class introduction:
+    '''
+        1. Introduction
+        2. Recursion
+        3. Cracking Interview Questions on Recursion
+        4. Big O Notation
+        5. Cracking Questions on Big O Notation
+        6. Arrays
+        7. Lists
+        8. Crack Interview Questions on Arrays and Lists
+        9. Dictionaries
+        10. Tuples
+        11. Linked Lists
+        12. Cracking Questions on Linked Lists
+        13. Stacks
+        14. Queues
+        15. Cracking Stack and Queues Interview Questions
+        16. Trees / Binary Trees
+        17. Binary Search Tree
+        18. AVL Tree
+        19. Binary Heap
+        20. TRIE
+        21. Hasing
+        22. SORT algorithms
+        23. Graph algorithms
+        24. Greedy Algorithms
+        25. Divide and Conquer algorithms
+        26. Dynamic programming
+        27. DP Exercises
+        28.The wild west
+
+    '''
+
+if __name__ == "__main__":
+    print(introduction().__doc__)
+    
