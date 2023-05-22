9b46c914d84de4ccd2990330685db8c8d51fa566
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 23:13:26 +0530

    Recursive vs Iterative solutions

---
+++ 02 - Recursion/02.4 Recursive vs Iterative Solutions.py
@@ -0,0 +1,10 @@
+class recursion:
+    '''
+      Recursive VS Iterative Method
+      space efficient?  No              Yes            (No stack memory is required in case of iteration)
+      Time efficient?   No              Yes            (In case if recursion system needs more time for pop and push elements to stack memory which makes recursion less time efficient)
+      Easy to code?     Yes             No             (We use recursion especially in the cases we know that a problem can be divided into similar sub problems)
+    '''
+
+if __name__ == "__main__":
+    print(recursion().__doc__)
