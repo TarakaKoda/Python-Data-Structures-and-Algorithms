96ea621aea7b4af96f382061dd072ef7c73775c3
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 23:12:34 +0530

    Why do we need recursion

---
+++ 02 - Recursion/02.2 Why do we need Recursion.py
@@ -0,0 +1,17 @@
+class recursion:
+    '''
+    Why recursion?
+      1. Recursive thinking is really important in programming, and it helps you break down big problems into smaller ones and easier to use
+      When to choose recursion
+      - If you can divine the problem into similar sub problems
+      - Design an algorithm to compute nth...
+      - write code to list the n....
+      - Implement a method ti compute all
+      - proactive
+      2. The prominent usage of recursion in data structures like trees and graphs
+      3. It is used in many algorithms(DAC,Greedy and DP)
+    '''
+
+
+if __name__ == "__main__":
+    print(recursion().__doc__)
