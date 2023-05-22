f5542b11f5a6ea50e5b6caa019f96dc5490c4d18
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 23:12:01 +0530

    Introduction to recursion

---
+++ 02 - Recursion/02.1 What is Recursion.py
@@ -0,0 +1,12 @@
+class recursion:
+    '''
+    What is recursion?
+      It is a way of solving a problem by having a function calling itself.
+      Properties of recursion:
+      - Performing the same operation multiple times with different inputs
+      - In every strip we try smaller inputs to male the problem smaller
+      - Base condition is needed to stop the recursion, otherwise infinite loop will occur.
+    '''
+
+if  __name__ == "__main__":
+    print(recursion().__doc__)
