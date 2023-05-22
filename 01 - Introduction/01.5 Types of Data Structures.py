3e3dcd11dbf0c69393777989b578aa640f339340
TarakaKoda <taraka.koda@gmail.com>
2023-05-22 22:08:13 +0530

    Types of Data Structures

---
+++ 01 - Introduction/01.5 Types of Data Structures.py
@@ -0,0 +1,16 @@
+class introduction:
+    '''
+    Types of Data structures:
+        Primitive Data structures and Non-primitive Data structures
+        Primitive - Integers, Float, Characters, String and Boolean
+        Non-Primitive - Derived from combining 1 or more primitive Data structures
+        Divided into two - Linear and Non-Linear
+        Linear - Static(Memory Location is Fixed) and Dynamic(Memory Location is changed)
+        Static - Array
+        Dynamic- Linked Lists, Stack, queue
+        Non-Linear - Not organized sequentially - Tree and Graphs
+        Each Data structure have its unique properties.
+    '''
+
+if __name__ == "__main__":
+    print(introduction().__doc__)
