fad9c3966e047bb3f60748d4c963212b99979e73
TarakaKoda <taraka.koda@gmail.com>
2023-05-24 23:40:18 +0530

    Big O, Big Theta and Big Omega

---
+++ 05 - Big O Notation/05.02 Big O, Big Theta and Big Omega.py
@@ -0,0 +1,13 @@
+class Big_O:
+    '''
+    Notations:
+      Big O : It is a complexity that is going to be less or equal to the worst case.
+      Big-Omega : It is a complexity that is going to be at least more than the best case.
+      Big Theta: It is a complexity that is within the bounds of the worst case and the best case.
+      Big O - O(N)
+      Big Omega : Omega(1)
+      Big Theta : Theta(n/2)
+    '''
+
+if __name__ == "__main__":
+    print(Big_O().__doc__)
