1afa98ba71e5ec2314c0af3f9569fbc862837f5d
TarakaKoda <taraka.koda@gmail.com>
2023-05-24 23:40:57 +0530

    Time complexity examples

---
+++ 05 - Big O Notation/05.03 Time complexity examples.py
@@ -0,0 +1,13 @@
+class Big_O:
+    '''
+    Time Complexity examples
+      O(1) Constant Accessing an specific element in array
+      O(N) Linear time complexity Loop through array elements
+      O(LogN) Logarithmic  Find an element in sorted array
+      O(N^2) Quadratic Looking for every index in the array twice.
+      O(2^N) Exponential Double recursion in fibonacci
+
+    '''
+
+if __name__ == "__main__":
+    print(Big_O().__doc__)
