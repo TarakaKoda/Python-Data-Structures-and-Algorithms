909673f3dadbf7b6f1094a4776d4c9d13d03ca9e
TarakaKoda <taraka.koda@gmail.com>
2023-05-24 23:39:25 +0530

    Analogy and Time Complexity

---
+++ 05 - Big O Notation/05.01 Analogy and Time Complexity.py
@@ -0,0 +1,13 @@
+class Big_O:
+    '''
+    Big O is the language and metric we use to describe the efficiency of algorithms.
+      Algorithm can perform differently based on the conditions given
+      Three scenarios in case of measuring of algorithms
+      - Best case
+      - Average case
+      - Worst case
+    '''
+
+
+if __name__ == "__main__":
+    print(Big_O().__doc__)
