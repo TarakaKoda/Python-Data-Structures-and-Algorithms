2a701b19e87ef6c8f6c28446c55b16c3aa74f31a
TarakaKoda <taraka.koda@gmail.com>
2023-05-24 23:45:00 +0530

    Add vs Multiply

---
+++ 05 - Big O Notation/05.06 Add vs Multiply.py
@@ -0,0 +1,9 @@
+class Big_O:
+    '''
+    Add VS Multiply
+    IF two individual loops then add the Runtimes.
+    If two loops are like do this for each then multiply.
+    '''
+
+if __name__ == "__main__":
+    print(Big_O().__doc__)
