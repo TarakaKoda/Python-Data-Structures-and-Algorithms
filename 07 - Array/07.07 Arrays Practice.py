c5d796ecefbfbd551b710dec0c9bd685293e915f
TarakaKoda <taraka.koda@gmail.com>
2023-05-25 23:34:31 +0530

    practice these array questions

---
+++ 07 - Array/07.07 Arrays Practice.py
@@ -0,0 +1,24 @@
+class Array:
+    '''
+    Arrays Practice
+      1. Create an array amd traverse
+      2. Access individual elements through indexes
+      3. Append any value to the array using append() method
+      4. Insert value to the array using insert() method
+      5. Extend python array using extend() method
+      6. Add items from list into array using fromlist() method
+      7. Remove any array element using remove() method
+      8. Remove last array element using pop() method
+      9. Fetch any element through its index using index() method
+      10. Reverse a python array using reverse() method
+      11. get attach buffer information through buffer_info() method
+      12. check for number of occurrences of an element using count() method
+      13. convert array to string using tostring() method
+      14. convert array to a python list with same elements using tolist() method
+      15. Append a string to char array using fromstring() method
+      16. Slice elements from an array
+    '''
+
+
+if __name__ == "__main__":
+    print(Array().__doc__)
