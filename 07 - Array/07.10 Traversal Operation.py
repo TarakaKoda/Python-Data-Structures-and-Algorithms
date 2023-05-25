ba53ebd1503c1d5d4d2ad8c57a2a84d60aea2b77
TarakaKoda <taraka.koda@gmail.com>
2023-05-25 23:38:21 +0530

    Traversal two-dimensional array

---
+++ 07 - Array/07.10 Traversal Operation.py
@@ -0,0 +1,16 @@
+class Array:
+    '''
+    Traversal
+      Time - O(mn)
+      space - O(1)
+      Search
+      Time - O(mn)
+      Space - O(1)
+      Delete
+      Time - O(mn)
+      space - O(1)
+
+    '''
+
+if __name__ == "__main__":
+    print(Array().__doc__)
