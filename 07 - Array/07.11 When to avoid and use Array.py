fdb56093f3d10690c4c050c8d54ae706c8edb842
TarakaKoda <taraka.koda@gmail.com>
2023-05-25 23:39:37 +0530

    When to use/avoid array

---
+++ 07 - Array/07.11 When to avoid and use Array.py
@@ -0,0 +1,13 @@
+class Array:
+    '''
+    When to use/avoid arrays
+      use
+      - To store multiple values of same data type
+      - Random access
+      Avoid
+      - Same data type elements
+      - Reserve memory
+    '''
+
+if __name__ == "__main__":
+    print(Array().__doc__)
