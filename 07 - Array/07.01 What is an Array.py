9aa677197697e4ae707c56b879d727b2fb2a6884
TarakaKoda <taraka.koda@gmail.com>
2023-05-25 23:30:23 +0530

    Introduction to Array

---
+++ 07 - Array/07.01 What is an Array.py
@@ -0,0 +1,16 @@
+class Array:
+    '''
+    Array is most used data structure. An array is simply collection of objections ot things. Very powerful data structure that store list of elements. end less applications.
+      Properties:
+      - Array can store data of specific type
+      - Elements of array are located in a contiguous memory location
+      - Each element of an array has a unique index
+      - The size of array is predefined and cannot be modified
+      An array is a data structure consisting of a collection of elements,each identified by at lease one array index or key. An array is stored such that position of each element can be computed from its index by a mathematical formula.
+      why do we need an array?
+      3 variables easy to declare, but to store 500 values we cannot declare 500 variables. Then comes array to help. All the 500 variables can be stored in an array.
+
+    '''
+
+if __name__ == "__main__":
+    print(Array().__doc__)
