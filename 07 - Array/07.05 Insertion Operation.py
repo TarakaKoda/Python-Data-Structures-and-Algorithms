091ceb4f0dcc947e6e43470f99b072fe2898cee4
TarakaKoda <taraka.koda@gmail.com>
2023-05-25 23:33:43 +0530

    Insertion operation in 1D

---
+++ 07 - Array/07.05 Insertion Operation.py
@@ -0,0 +1,15 @@
+class Array:
+    '''
+     Insertion
+      arr[index] = value
+      Insert at beginning
+      Move all elements one to right and insert at start
+      What if array is full
+      we need to create a new array and copy then perform the operation
+      Time - O(N) at beginning or middle and O(1) at end
+      Space - O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(Array().__doc__)
