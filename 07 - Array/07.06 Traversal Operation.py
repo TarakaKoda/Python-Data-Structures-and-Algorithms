bc8f185f7017a8cc5ab8b2a309db2132dc3b6ba1
TarakaKoda <taraka.koda@gmail.com>
2023-05-25 23:34:03 +0530

    Traversal operation in 1D

---
+++ 07 - Array/07.06 Traversal Operation.py
@@ -0,0 +1,23 @@
+class Array:
+    '''
+    Traversal
+      visiting all cells of array till the end
+      for i in array:
+        print(i)
+      Time - O(N)
+      space - O(1)
+      Access array element
+      How can we tell the computer which particular value we would like to access?
+      Index - array[index]
+      Time - O(1)
+      Space - O(1)
+      Finding an element
+      Time - O(n)
+      Space - O(1)
+      Deletion of an element
+      time - O(n)
+      space - O(1)
+    '''
+
+if __name__ == "__main__":
+    print(Array().__doc__)
