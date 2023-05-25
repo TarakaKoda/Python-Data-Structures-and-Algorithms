62842e2c990bb254943c5c13a170d8622faa1a1f
TarakaKoda <taraka.koda@gmail.com>
2023-05-25 23:31:10 +0530

    Array in memory

---
+++ 07 - Array/07.03 Arrays in Memory.py
@@ -0,0 +1,16 @@
+class Array:
+    '''
+      Memory
+      stored in contagious location
+      two Dimensional
+      [[1,2,3],[4,5,6],[7,8,9]]
+      Memory
+      stored in contagious location
+      three Dimensional
+      [[[0,1,2],[3,4,5]],[[6,7,8],[9,10,11]],[[12,13,14]]]
+      Memory
+      stored in contagious location next to each other
+    '''
+
+if __name__ == "__main__":
+    print(Array().__doc__)
