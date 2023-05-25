29c013d521c428eba6e375c807d5e81c4d865626
TarakaKoda <taraka.koda@gmail.com>
2023-05-25 23:30:44 +0530

    Types of array

---
+++ 07 - Array/07.02 Types of Array.py
@@ -0,0 +1,14 @@
+class Array:
+    '''
+    Types of array
+      One Dimensional - an array with a bunch of values haveing been declared with a single index
+      a[i] -> i between 0 and n
+      [1,2,3,4,5]
+      Multi Dimensional
+      Two Dimensional am array with a bunch of values having been decided with double index
+      a[i][j] - i and j between 0 and n
+    '''
+
+
+if __name__ == "__main__":
+    print(Array().__doc__)
