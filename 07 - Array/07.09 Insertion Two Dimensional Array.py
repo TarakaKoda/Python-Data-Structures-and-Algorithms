f0ba09dc00765d4d6bbff42121433bc287e1cafd
TarakaKoda <taraka.koda@gmail.com>
2023-05-25 23:37:59 +0530

    Insertion two-dimensional array

---
+++ 07 - Array/07.09 Insertion Two Dimensional Array.py
@@ -0,0 +1,14 @@
+class Array:
+    '''
+     Insertion
+      Time - O(mn)/O(1)
+      space - O(1)
+      Accessing
+      arr[row][column]
+      time - O(1)
+      space - O(1)
+
+    '''
+
+if __name__ == "__main__":
+    print(Array().__doc__)
