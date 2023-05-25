b5a96bb3173e69c484c5d3c2ac8b3fed68b3b40f
TarakaKoda <taraka.koda@gmail.com>
2023-05-25 23:36:37 +0530

    Two dimensional array

---
+++ 07 - Array/07.08 Two Dimensional Array.py
@@ -0,0 +1,14 @@
+class Array:
+    '''
+      Two Dimensional arrays
+      numpy module
+      Rows and columns
+      arr[i][j]
+      Create
+      Time - O(1)
+      Space - O(mn)
+    '''
+
+
+if __name__ == "__main__":
+    print(Array().__doc__)
