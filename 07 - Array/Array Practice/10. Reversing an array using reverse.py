f6aede473fefae461da8e70db2046909bdcc7f24
TarakaKoda <taraka.koda@gmail.com>
2023-05-26 23:51:04 +0530

    Reverse a python array using reverse() method

---
+++ 07 - Array/Array Practice/10. Reversing an array using reverse.py
@@ -0,0 +1,15 @@
+# 10. Reverse a python array using reverse() method
+from array import *
+
+class Array:
+
+    def reverse_an_array_using_reverse(self):
+        my_array = array('i',[1,2,3,4,5])
+        print("Step 10")
+        my_array.reverse()
+        print(my_array)
+
+
+
+if __name__ == "__main__":
+    Array().reverse_an_array_using_reverse()
