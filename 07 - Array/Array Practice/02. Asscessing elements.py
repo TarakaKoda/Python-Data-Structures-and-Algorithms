b2bafff2598fad007a7f2ab7ef732d203aec4ed7
TarakaKoda <taraka.koda@gmail.com>
2023-05-26 23:48:29 +0530

    Accessing individual elements through indexes

---
+++ 07 - Array/Array Practice/02. Asscessing elements.py
@@ -0,0 +1,14 @@
+# 2. Access individual elements through indexes
+from array import *
+
+class Array:
+
+    def acccess_single_elements_through_indexes(self):
+        my_array = array('i',[1,2,3,4,5])
+        print("Step 2")
+        print(my_array[3])
+
+
+
+if __name__ == "__main__":
+    Array().acccess_single_elements_through_indexes()
