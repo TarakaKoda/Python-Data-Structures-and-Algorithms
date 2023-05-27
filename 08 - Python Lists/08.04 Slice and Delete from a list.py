a419fc1f6da1b0508785633aaaddb98c411a4a5c
TarakaKoda <taraka.koda@gmail.com>
2023-05-27 22:46:11 +0530

    Slicing and Deleting from a List

---
+++ 08 - Python Lists/08.04 Slice and Delete from a list.py
@@ -0,0 +1,9 @@
+class Lists:
+    '''
+      Delete - remove(value) Time O(n) space - O(1), pop(index) - returns the deleted item if no index delete last Time O(1) space - O(1), del lis[index], del lis[index_start:index_end]
+      reverse - reverse() Time O(n) space - O(1)
+      slice - slice[:] start:end:step - end exclusive
+    '''
+
+if __name__ == "__main__":
+    print(Lists().__doc__)
