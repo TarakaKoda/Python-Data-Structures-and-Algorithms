695c26ed3e7b2ba3cb5dbd56cc3824d8bf69b78a
TarakaKoda <taraka.koda@gmail.com>
2023-05-27 22:51:43 +0530

    Putting our learning into practice: A comprehensive exercise

---
+++ 08 - Python Lists/List Quiz/01. Question.py
@@ -0,0 +1,13 @@
+class Lists:
+    '''
+    What will be the output of the following code block?
+    '''
+
+    def slicing(self):
+        a = [1,2,3,4,5,6,7,8,9,10]
+        print(a[::2])
+
+
+if __name__ == "__main__":
+    print(Lists().__doc__)
+    # Lists().slicing()
