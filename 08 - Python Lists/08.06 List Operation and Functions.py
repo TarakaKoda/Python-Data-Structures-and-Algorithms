cdcba0091bd239e56bf538303577e519bac51eca
TarakaKoda <taraka.koda@gmail.com>
2023-05-27 22:47:04 +0530

    List Operations and Function

---
+++ 08 - Python Lists/08.06 List Operation and Functions.py
@@ -0,0 +1,13 @@
+class Lists:
+    '''
+    List Operations/Functions
+      + operator: concatenate lists
+      * operator: multiplying list(repetitive function)
+      len: count the number of items in the list
+      max: to get maximum of items in the list
+      min: to get minimum of items in the list
+      sum: to get sum of items in the list
+    '''
+
+if __name__ == "__main__":
+    print(Lists().__doc__)
