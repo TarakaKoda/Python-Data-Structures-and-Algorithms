36f8e3e4b6085bc07b44ebfb4f07b2bebb035583
TarakaKoda <taraka.koda@gmail.com>
2023-05-27 22:45:09 +0530

    Accessing/Traversing a List

---
+++ 08 - Python Lists/08.02 Accessing Traversing a list.py
@@ -0,0 +1,10 @@
+class Lists:
+    '''
+      Accessing Time - O(1) space - O(1)
+      Traversal Time - O(n) space - O(1)
+      lis[-1] : last item in list .....
+      lis[1-n] : first item in list .....
+    '''
+
+if __name__ == "__main__":
+    print(Lists().__doc__)
