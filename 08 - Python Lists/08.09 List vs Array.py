9156ea0e1025a0fe48d912df0b1305b380684f63
TarakaKoda <taraka.koda@gmail.com>
2023-05-27 22:48:35 +0530

    List vs Array

---
+++ 08 - Python Lists/08.09 List vs Array.py
@@ -0,0 +1,17 @@
+class Lists:
+    '''
+      Similarities
+      Both data structures are mutable
+      Both can be indexed and iterated through
+      They can be both sliced
+
+      Arithmetic operations
+      array/2 works
+      list/2 - Doesn't
+      Data types
+      array - only same data types
+      list - any data types
+    '''
+
+if __name__ == "__main__":
+    print(Lists().__doc__)
