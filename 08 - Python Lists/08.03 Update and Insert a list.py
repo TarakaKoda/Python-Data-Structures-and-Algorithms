23e261bf0dcc74bf824053aede0498866b5c9953
TarakaKoda <taraka.koda@gmail.com>
2023-05-27 22:45:31 +0530

    Update/Insert a List

---
+++ 08 - Python Lists/08.03 Update and Insert a list.py
@@ -0,0 +1,8 @@
+class Lists:
+    '''
+      Insertion - Time O(n) space - O(1) -insert(),  Time O(1) space - O(1)append(),  Time O(1) space - O(n)extend()
+      Updating - Time O(1) space - O(1)
+    '''
+
+if __name__ == "__main__":
+    print(Lists().__doc__)
