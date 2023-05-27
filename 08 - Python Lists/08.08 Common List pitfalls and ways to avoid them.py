29bf5a864cb505cf044605196620d06afa0c907c
TarakaKoda <taraka.koda@gmail.com>
2023-05-27 22:48:16 +0530

    Read the documentation of list functions carefully and see if it is returning null or a modified version of list

---
+++ 08 - Python Lists/08.08 Common List pitfalls and ways to avoid them.py
@@ -0,0 +1,9 @@
+class Lists:
+    '''
+      Common list pitfalls and ways to avoid them
+      Read the documentation of list functions carefully and see if it is returning null or an modified version of list
+    '''
+
+if __name__ == "__main__":
+    print(Lists().__doc__)
+
