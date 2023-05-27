d8924672fc4d2531ed63d71c462311935b3d2e8c
TarakaKoda <taraka.koda@gmail.com>
2023-05-27 22:52:08 +0530

    Putting our learning into practice: A comprehensive exercise

---
+++ 08 - Python Lists/List Quiz/04. Question.py
@@ -0,0 +1,24 @@
+class Lists:
+    '''
+    What will be the output of the following code snippet?
+
+    A. 1 44
+    B. 3 1
+    C. 3 44
+    D. 1 1
+    '''
+
+    def values(vales, values):
+        v = 1
+        values[0] = 44
+
+
+t = 3
+v = [1,2,3]
+
+list = Lists
+
+if __name__ == "__main__":
+    list.values(t, v)
+    print(Lists().__doc__)
+    # print(t, v[0])
