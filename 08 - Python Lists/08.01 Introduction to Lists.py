fb8394f4fd4c354b71a040c1f7d1c2d44df4b50d
TarakaKoda <taraka.koda@gmail.com>
2023-05-27 22:44:30 +0530

    Introduction to Lists

---
+++ 08 - Python Lists/08.01 Introduction to Lists.py
@@ -0,0 +1,9 @@
+class Lists:
+    '''
+      A list is a data structure that holds an ordered collection of items.
+      list - [1,'name',[30,10]]
+      nested list - list inside list
+    '''
+
+if __name__ == "__main__":
+    print(Lists().__doc__)
