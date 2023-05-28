41c3b2314e5d66fc4a62ee4543f3aa170c964d9f
TarakaKoda <taraka.koda@gmail.com>
2023-05-28 22:52:45 +0530

    List of interview question (Array and List)

---
+++ 10 - Array and List Interview Questions/10.00 List of questions.py
@@ -0,0 +1,14 @@
+class Interview_Question:
+    '''
+    Question 1: Missing Number How to find the missing number in an integer array of 1 to 100?
+    sum of n number = n*(n+1)/2 Time - O(n) space - O(1)
+    Question 2: write a program to find all pairs of integer whose sum is equal to a given number?
+    Question 3: How to check if an array contains a number in python?
+    Question 4: Find max product of integers in an array.
+    Question 5: Implement an algorithm to determine if a list has all unique characters using python list.
+    Question 6: Permutations
+    Question 7: Given an image represented by an N*N matrix write a program to rotate the image by 90 degrees
+    '''
+
+if __name__ == "__main__":
+    print(Interview_Question().__doc__)
