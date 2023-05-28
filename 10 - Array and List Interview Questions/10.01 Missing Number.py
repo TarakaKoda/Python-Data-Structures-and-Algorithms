0ae52d901e2f719d52317549a7fefb44f734de66
TarakaKoda <taraka.koda@gmail.com>
2023-05-28 22:53:01 +0530

    How to find the missing number in an integer array of 1 to 100?

---
+++ 10 - Array and List Interview Questions/10.01 Missing Number.py
@@ -0,0 +1,22 @@
+class Interview_question1:
+    '''
+    How to find the missing number in an integer array of 1 to 100?
+    sum of n number = n*(n+1)/2 Time - O(n) space - O(1)
+    '''
+
+    # def missingNumber(self,lis,n):
+    #     sum = n*(n+1)/2
+    #     lis_sum = sum(lis)
+    #     return sum - lis_sum
+
+    def missingNumber(self, list, n):
+        sum1 = sum(list)
+        sum2 = 100 * 101 // 2
+        print(sum2 - sum1)
+
+mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+
+
+if __name__ == "__main__":
+    print(Interview_question1().__doc__)
+    Interview_question1().missingNumber(mylist, 100)
