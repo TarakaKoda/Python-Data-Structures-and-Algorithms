829d11b5dc379b7154f6e13a01d9440ab165fafe
TarakaKoda <taraka.koda@gmail.com>
2023-05-29 23:11:20 +0530

    Dictionary operations and built-in functions

---
+++ 11 - Dictionaries/11.09 Dictionary operations and Built in Functions.py
@@ -0,0 +1,14 @@
+class Dictionary:
+    '''
+      Dictionary operations/Built in Functions
+      in operator - checks if key exists in the dictionary
+      for operator - loop through dictionary
+      all(dictionary) - If all elements in the dictionary are true
+      any(dict)  - Empty/All values are false False others true
+      len(dict) no of pairs in the dictionary
+      sorted(dict,reverse,key)
+    '''
+
+if __name__ == "__main__":
+    print(Dictionary().__doc__)
+
