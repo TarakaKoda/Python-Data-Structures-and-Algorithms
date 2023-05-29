0ae3faebaac5373d3ada171c26a66e95f30b2d85
TarakaKoda <taraka.koda@gmail.com>
2023-05-29 23:10:47 +0530

    Dictionary methods

---
+++ 11 - Dictionaries/11.08 Dictionary methods.py
@@ -0,0 +1,18 @@
+class Dictionary:
+    '''
+      Dictionary Methods:
+      clear()
+      cop()
+      fromkeys(sequence[],value) - new dictionary
+      get(key,value)
+      items()
+      keys()
+      popitem()
+      setDefault(key,default_value)
+      pop(key,default_value)
+      values()
+      update(other_dict)
+    '''
+
+if __name__ == "__main__":
+    print(Dictionary().__doc__)
