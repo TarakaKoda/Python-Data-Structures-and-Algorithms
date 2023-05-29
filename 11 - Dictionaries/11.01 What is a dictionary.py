bba65b0c36b9614948b484b1f8b487a07f76bd1a
TarakaKoda <taraka.koda@gmail.com>
2023-05-29 23:07:07 +0530

    Introduction to Dictionaries

---
+++ 11 - Dictionaries/11.01 What is a dictionary.py
@@ -0,0 +1,10 @@
+class Dictionary:
+    '''
+      A Dictionary is a collection which is unordered, changeable and indexed.
+      Key value pair. Miller: a person who works or own a mill
+      dict = {'name': 'John', 'age': 18, 'gender': 'male'}
+      dict['name'] - John
+    '''
+
+if __name__ == "__main__":
+    print(Dictionary().__doc__)
