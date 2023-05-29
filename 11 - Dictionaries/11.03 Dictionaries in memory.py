50d9047f6ca47557d8952bd94a3c8c82658b7e6e
TarakaKoda <taraka.koda@gmail.com>
2023-05-29 23:07:51 +0530

    Dictionaries in memory

---
+++ 11 - Dictionaries/11.03 Dictionaries in memory.py
@@ -0,0 +1,9 @@
+class Dictionary:
+    '''
+    Dictionaries in Memory
+    A hash table is a way of doing key-value lookups. You store the values in an array and then use a hash function to
+    find the index of the array cell that corresponds to your key-value pair.Key array and hash function.
+    '''
+
+if __name__ == "__main__":
+    print(Dictionary().__doc__)
