46798ff722e01e5920b74b18166cf2218dec8bfd
TarakaKoda <taraka.koda@gmail.com>
2023-05-29 23:11:36 +0530

    Dictionary vs List

---
+++ 11 - Dictionaries/11.10 Dictionary vs List.py
@@ -0,0 +1,14 @@
+class Dictionary:
+    '''
+      Dictionary                                   List
+      Unordered                                    Ordered
+      Access via keys                              Access via index
+      collection of key value pairs                collection of elements
+      preferred when you have unique key values    Preferred when you have ordered data
+      No duplicate members                         Allow duplicate members
+
+    '''
+
+
+if __name__ == "__main__":
+    print(Dictionary().__doc__)
