d16ed836db0ee0e53e6faad977c9128b6f5d44f9
TarakaKoda <taraka.koda@gmail.com>
2023-05-29 23:08:44 +0530

    Inserting and Updating a Dictionary

---
+++ 11 - Dictionaries/11.04 Insert and Update.py
@@ -0,0 +1,10 @@
+class Dictionary:
+    '''
+      Insert/Update an element in the dictionary
+      dict['key'] = 'value' It will update if the key already exists or adds the new key value to the dictionary if it
+      doesn't already exist.
+      Time - O(1)
+    '''
+
+if __name__ == "__main__":
+    print(Dictionary().__doc__)
