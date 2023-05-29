4e8d22777a5fd107da7edc8f9d31dbf6e5a81006
TarakaKoda <taraka.koda@gmail.com>
2023-05-29 23:10:31 +0530

    Delete and Remove an element from the dictionary

---
+++ 11 - Dictionaries/11.07 Delete and Remove an element from the dictionary.py
@@ -0,0 +1,14 @@
+class Dictionary:
+    '''
+      Delete/Remove an element from the dictionary
+      pop() - removes the element from the dictionary based on its key and returns the value
+      popitem() - deletes random element from the dictionary and return key and value
+      clear() - clears the dictionary
+      del - deletes the Dictionary based on the key
+      Time - O(1)-Average/Amortized O(n)
+      Space - O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(Dictionary().__doc__)
