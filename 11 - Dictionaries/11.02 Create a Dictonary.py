768b59570a8349c1b2a47ee7c06c04e4a5824f47
TarakaKoda <taraka.koda@gmail.com>
2023-05-29 23:07:26 +0530

    Create a Dictionary

---
+++ 11 - Dictionaries/11.02 Create a Dictonary.py
@@ -0,0 +1,11 @@
+class Dictonary:
+    '''
+      Creating a Dictionary
+      dict() function gives an empty dictionary initalized
+      {} also initializes dictionary
+      Time - O(1)
+      Space - O(n)
+    '''
+
+if __name__ == "__main__":
+    print(Dictonary().__doc__)
