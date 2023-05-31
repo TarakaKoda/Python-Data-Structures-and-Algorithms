2c2bc5d2581e2167a4ef8881e2ccd04398e6e639
TarakaKoda <taraka.koda@gmail.com>
2023-05-31 23:20:28 +0530

    Time and Space Complexity of Tuple

---
+++ 12 - Tuple/12.07 Time and Space complexity of Tuples.py
@@ -0,0 +1,14 @@
+class Tuple:
+    '''
+         ---------------------------------------------------------------------------------------------
+                Operation                     Time Complexity                         Space Complexity
+         ---------------------------------------------------------------------------------------------
+         Creating a Tuple                       O(1)                                       O(n)
+         Traversing a given Tuple               O(n)                                       O(1)
+         Accessing a given cell                 O(1)                                       O(1)
+         Searching a given value                O(n)                                       O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(Tuple().__doc__)
