488c959199682474ead0f750fd9de60c9219965d
TarakaKoda <taraka.koda@gmail.com>
2023-05-31 23:46:25 +0530

    Putting our learning into practice: A comprehensive exercise

---
+++ 12 - Tuple/Tuple Quiz/01. Question.py
@@ -0,0 +1,19 @@
+class Tuple:
+    '''
+    What will be the output of the following code block?
+
+    A. None
+    B.  1
+    C. 0
+    D. Exception
+    '''
+
+    def check_tuple(self):
+        init_tuple = ()
+        print(init_tuple.__len__())
+
+
+
+if __name__ == "__main__":
+    print(Tuple().__doc__)
+    # Tuple().check_tuple()
