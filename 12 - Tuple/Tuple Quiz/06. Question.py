b298c7f6d37233c926c858e0725d22d1f0a4e052
TarakaKoda <taraka.koda@gmail.com>
2023-05-31 23:47:04 +0530

    Putting our learning into practice: A comprehensive exercise

---
+++ 12 - Tuple/Tuple Quiz/06. Question.py
@@ -0,0 +1,13 @@
+class Tuple:
+    '''
+    Which of the following statements given below is/are true?
+
+    A. Tuples have structure, lists have an order.
+    B. Tuples are homogeneous, lists are heterogeneous.
+    C. Tuples are immutable, lists are mutable.
+    D. All of them.
+    '''
+
+
+if __name__ == "__main__":
+    print(Tuple().__doc__)
