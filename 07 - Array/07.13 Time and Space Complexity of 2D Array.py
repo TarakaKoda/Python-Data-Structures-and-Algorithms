b7653c9f6bd3f827ac82da469509898d22449d4c
TarakaKoda <taraka.koda@gmail.com>
2023-05-31 23:58:58 +0530

    Time and Space Complexity of 2D Array

---
+++ 07 - Array/07.13 Time and Space Complexity of 2D Array.py
@@ -0,0 +1,16 @@
+class Two_D_Array:
+    '''
+         ---------------------------------------------------------------------------------------------
+                Operation                     Time Complexity                         Space Complexity
+         ---------------------------------------------------------------------------------------------
+         Creating a empty Array                 O(1)                                       O(mn)
+         Inserting a Row/Column in an 2D array  O(1)/O(mn)                                 O(1)
+         Traversing a given Tuple               O(mn)/O(n^2)                               O(1)
+         Accessing a given cell                 O(1)                                       O(1)
+         Searching a given cell                 O(mn)/O(n^2)                               O(1)
+         Deleting a given cell                  O(1)/O(mn)                                 O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(Two_D_Array().__doc__)
