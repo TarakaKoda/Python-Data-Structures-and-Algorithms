29c11d6a4906fc43384884dc0c705a8c63de4145
TarakaKoda <taraka.koda@gmail.com>
2023-05-31 23:19:46 +0530

    Tuple operations and methods

---
+++ 12 - Tuple/12.05 Tuple OperationsFunctions.py
@@ -0,0 +1,16 @@
+class Tuple:
+    '''
+    Tuple Operations and methods
+    + : concatenation
+    * : repeat add
+    in: search
+    count(): gives count of an element
+    len(): gives the length of tuple
+    max() - gives maximum of the tuple
+    min() - gives minimum of the tuple
+    tuple() - used to convert a list into tup
+    '''
+
+
+if __name__ == "__main__":
+    print(Tuple().__doc__)
