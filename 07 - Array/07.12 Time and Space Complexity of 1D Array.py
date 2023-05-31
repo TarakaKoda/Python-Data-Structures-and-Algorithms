f641f40d7cf7625284ec11abde041b0833123b3d
TarakaKoda <taraka.koda@gmail.com>
2023-05-31 23:58:45 +0530

    Time and Space Complexity of 1D Array

---
+++ 07 - Array/07.12 Time and Space Complexity of 1D Array.py
@@ -0,0 +1,16 @@
+class One_D_Array:
+    '''
+         ---------------------------------------------------------------------------------------------
+                Operation                     Time Complexity                         Space Complexity
+         ---------------------------------------------------------------------------------------------
+         Creating a empty Array                 O(1)                                       O(n)
+         Inserting a value in an array          O(1)/O(n)                                  O(1)
+         Traversing a given Tuple               O(n)                                       O(1)
+         Accessing a given cell                 O(1)                                       O(1)
+         Searching a given cell                 O(n)                                       O(1)
+         Deleting a given cell                  O(1)/O(n)                                  O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(One_D_Array().__doc__)
