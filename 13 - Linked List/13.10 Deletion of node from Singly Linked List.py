3215611c1582815b80139304ae1b3ea263a0d918
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:41:47 +0530

    Deleting a node in a Singly Linked List

---
+++ 13 - Linked List/13.10 Deletion of node from Singly Linked List.py
@@ -0,0 +1,15 @@
+class Linked_List:
+    '''
+    Delete
+      - Deleting the first node Time O(1) Space O(1)
+      - Deleting any given node Time O(N) space O(1)
+      - Deleting the last node Time O(N) Space O(1)
+      Algorithm
+    '''
+
+
+
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
