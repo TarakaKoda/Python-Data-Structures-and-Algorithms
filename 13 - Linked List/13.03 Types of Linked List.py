86fb7deffff63e7c1eec5c51ae371af3613de051
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:37:51 +0530

    Types of Linked List

---
+++ 13 - Linked List/13.03 Types of Linked List.py
@@ -0,0 +1,12 @@
+class Linked_List:
+    '''
+    Different types of linked lists
+      - Single linked list
+      - Doubly linked list
+      - Circular linked list
+      - Circular Doubly linked list
+    '''
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
