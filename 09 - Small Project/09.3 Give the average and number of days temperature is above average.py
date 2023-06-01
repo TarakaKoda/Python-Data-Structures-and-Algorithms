721373263ab7c67abcb9e8f8f2db470329327571
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:48:40 +0530

    Give the average and number of days temperature

--- 09 - Small Project/09.3 Give the average and number of days temperature is above average.py
+++ 09 - Small Project/09.3 Give the average and number of days temperature is above average.py
@@ -1,6 +1,19 @@
 class Array:
     '''
     Give the average and number of days temperature is above average
+
+    num = int(input("How many day's temperature? "))
+        my_list = []
+        for i in range(1, num + 1):
+            temp = int(input(f"Enter the maximum temperature of Day {i}: "))
+            my_list.append(temp)
+        average = round(sum(my_list) / num, 2)
+        above_average = 0
+        for i in my_list:
+            if i > average:
+                above_average += 1
+        return f"Average: {average}\n{above_average} day(s) are above average temperature"
+
     '''
 
 if __name__ == "__main__":
