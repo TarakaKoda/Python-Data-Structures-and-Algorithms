7d910adbcff6382ea629beb27ece2471d0700296
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:38:07 +0530

    Linked list in memory

---
+++ 13 - Linked List/13.04 Linked List in the Memory.py
@@ -0,0 +1,9 @@
+class Linked_List:
+    '''
+      Linked list in memory
+      Not located continuously. They are stored randomly.
+    '''
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
