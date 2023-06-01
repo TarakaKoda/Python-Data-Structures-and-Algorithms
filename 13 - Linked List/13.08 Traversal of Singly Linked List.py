60cebcbb0071d47f598f4a3e41ed3a202ef2d2a2
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:40:44 +0530

    Traversing Singly Linked List

---
+++ 13 - Linked List/13.08 Traversal of Singly Linked List.py
@@ -0,0 +1,11 @@
+class Linked_List:
+    '''
+    Traversal
+      Start -> check head -Yes-> Loop all nodes and print value -> Terminate
+            -> check head -No-> Terminate
+      Time - O(n) space - O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
