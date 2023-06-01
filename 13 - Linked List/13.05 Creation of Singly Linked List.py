27c8b1c96b16c1bccfc33b518cfbc04fb34cbb4c
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:38:33 +0530

    Creation of Single linked lists

---
+++ 13 - Linked List/13.05 Creation of Singly Linked List.py
@@ -0,0 +1,13 @@
+class Linked_List:
+    '''
+        Creation of Single linked lists
+      - Create head and tail, initialize with null
+      - Create a blank node and assign a value to it and reference to null
+      - Link head and tail with these node
+      Time - O(1)
+      Space - O(n) based on the number of items in the linked list
+    '''
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
