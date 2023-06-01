a70a9c9b5134cec64469cae0a4ebeb301d18f454
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:39:43 +0530

    Insertion algorithm

--- 13 - Linked List/13.07 Insertion in Singly Linked List Algorithm.py
+++ 13 - Linked List/13.07 Insertion in Singly Linked List Algorithm.py
@@ -1,6 +1,6 @@
 class Linked_List:
     '''
-    Insertion algorith
+    Insertion algorithm
       (Node value,location)  -> create a node and assign value -> head = None? -Yes-> head = node tail = node -> Terminate
       -No-> Location = first -No-> location=last -No-> Find location(loop) current.next = node node.next = nextNode -> Terminate
       location = first -Yes-> node.next = head head = node -> Terminate
