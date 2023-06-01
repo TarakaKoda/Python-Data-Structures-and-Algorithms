751ddbab7df5c0e04157f4296d1b094c69767711
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:42:06 +0530

    Deleting of entire Singly Linked List

---
+++ 13 - Linked List/13.11 Deletion of entire Singly Linked List.py
@@ -0,0 +1,14 @@
+class Linked_List:
+    '''
+      Delete all elements in Linked list
+      head = tail = none
+      Time - O(1)
+      Space - O(1)
+    '''
+
+
+
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
