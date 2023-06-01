1e5bc2c9ecb42e775bc56dcf75f5a57ebf4689a4
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:42:42 +0530

    Time and Space Complexity of Singly Linked List

---
+++ 13 - Linked List/13.12 Time and Space Complexity of Singly Linked List.py
@@ -0,0 +1,22 @@
+class Linked_List:
+    '''
+      Time Complexity Array VS Linked List
+      creation O(1) O(1)
+      Insertion at 1st position O(1) O(1)
+      Insertion at last position O(1) O(1)
+      insertion at nth position O(1) O(n)
+      Searching in unsorted data O(n) O(n)
+      searching in sorted data O(log n) O(n)
+      Traversing O(n) O(n)
+      Deleting at 1st position O(1) O(1)
+      Deleting at last position O(1) O(n)/O(1)
+      Deleting at nth position O(1) O(n)
+      Deleting an array/Linked list O(n) O(n)/O(1)
+      Accessing nth element O(1) O(n)
+    '''
+
+
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
