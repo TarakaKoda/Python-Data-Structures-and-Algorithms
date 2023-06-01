eee24e94030fff2b69d8415ede8b4e0d67835497
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:37:30 +0530

    Linked List vs Array

---
+++ 13 - Linked List/13.02 Linked List vs Arrays.py
@@ -0,0 +1,12 @@
+class Linked_List:
+    '''
+      Array VS Linkedlist
+      Elements of linked list are independent objects
+      Size of linked list is not predefined
+      Insertion and removals in linked list are very efficient
+      Random access - accessing an element is very efficient in arrays
+    '''
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
