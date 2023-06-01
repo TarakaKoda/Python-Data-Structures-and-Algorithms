6f9620d10928569f988831f397e6b037a592e298
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:36:02 +0530

    Introduction To Linked List

---
+++ 13 - Linked List/13.01 What is a Linked List.py
@@ -0,0 +1,12 @@
+class Linked_List:
+    '''
+      Linked list is a form of a sequential collection and it does not have to be in order. A lined list is made up of independent nodes that may contain any type of data and each node has a reference to the next node in the link.
+      Head, Tail, Nodes and links
+      Each node is independent
+      If we need to access a node we need to traverse through all previous nodes
+      node -data and reference
+    '''
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
