45696f6aaa68625a424d6233039f65a01630c3bf
TarakaKoda <taraka.koda@gmail.com>
2023-06-01 23:48:17 +0530

    Get the temperature recorded every day

--- 09 - Small Project/09.2 Get the temperature recorded every day.py
+++ 09 - Small Project/09.2 Get the temperature recorded every day.py
@@ -1,6 +1,12 @@
 class Array:
     '''
     Get the temperature recorded every day
+
+    num = int(input("How many day's temperature? "))
+        my_list = []
+        for i in range(1, num + 1):
+            temp = int(input(f"Enter the maximum temperature of Day {i}: "))
+            my_list.append(temp)
     '''
 
 if __name__ == "__main__":
