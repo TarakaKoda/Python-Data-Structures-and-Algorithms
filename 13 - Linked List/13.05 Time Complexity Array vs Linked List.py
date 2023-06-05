501f419384ca1f38a8c5aa30eac0d7ac1a6eaf06
TarakaKoda <taraka.koda@gmail.com>
2023-06-05 21:54:58 +0530

    Time Complexity Array vs Linked List

---
+++ 13 - Linked List/13.05 Time Complexity Array vs Linked List.py
@@ -0,0 +1,23 @@
+class Linked_List:
+    '''
+
+         -------------------------------------------------------------------------------------------------------
+                Operation                                  Array                                   Linked List
+         -------------------------------------------------------------------------------------------------------
+         Creation                                           O(1)                                       O(1)
+         Insertion at 1st position                          O(1)                                       O(1)
+         Insertion at last position                         O(1)                                       O(1)
+         insertion at nth position                          O(1)                                       O(N)
+         Searching in unsorted data                         O(N)                                       O(N)
+         Searching in sorted data                           O(log N)                                   O(N)
+         Traversing                                         O(N)                                       O(N)
+         Accessing a value                                  O(1)                                       O(N)
+         Deleting first position                            O(1)                                       O(1)
+         Deleting nth position                              O(1)                                       O(N)
+         Deleting last position                             O(1)                                       O(N)/O(1)
+         Deleting entire Array/Linked List                  O(1)                                       O(N)/O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
