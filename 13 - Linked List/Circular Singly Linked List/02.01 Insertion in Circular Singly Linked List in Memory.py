bdfbf6731adc51bcecc36b0c24f2a34db6a118e2
TarakaKoda <taraka.koda@gmail.com>
2023-06-05 22:03:56 +0530

    Insertion in a Circular Singly Linked List

--- 13 - Linked List/Circular Singly Linked List/02.01 Insertion in Circular Singly Linked List in Memory.py
+++ 13 - Linked List/Circular Singly Linked List/02.01 Insertion in Circular Singly Linked List in Memory.py
@@ -1,6 +1,9 @@
 class Linked_List:
     '''
     Insertion to Circular Singly Linked List
+    - Inserting into a Circular Single Linked List is same as the Singly Linked List but we need to change the last node
+      reference to the first node/newly inserted node.
+    - We can insert into Circular Singly Linked List in 3 ways:
       - At the beginning of the circular singly linked list Time - O(1) space - O(1)
       - After a node in the middle of circular singly linked list Time - O(n) space - O(1)
       - At the end of the circular singly linked list Time - O(1) space - O(1)
