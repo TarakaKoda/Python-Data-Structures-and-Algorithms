8790ca56508b2e6a72b6da39e61cea107761074e
TarakaKoda <taraka.koda@gmail.com>
2023-06-05 21:58:43 +0530

    Time and Space Complexity of a Singly Linked List

--- 13 - Linked List/Singly Linked List/06.00 Time and Space Complexity of Singly Linked List.py
+++ 13 - Linked List/Singly Linked List/06.00 Time and Space Complexity of Singly Linked List.py
@@ -1,18 +1,19 @@
 class Linked_List:
     '''
-      Time Complexity Array VS Linked List
-      creation O(1) O(1)
-      Insertion at 1st position O(1) O(1)
-      Insertion at last position O(1) O(1)
-      insertion at nth position O(1) O(n)
-      Searching in unsorted data O(n) O(n)
-      searching in sorted data O(log n) O(n)
-      Traversing O(n) O(n)
-      Deleting at 1st position O(1) O(1)
-      Deleting at last position O(1) O(n)/O(1)
-      Deleting at nth position O(1) O(n)
-      Deleting an array/Linked list O(n) O(n)/O(1)
-      Accessing nth element O(1) O(n)
+         -------------------------------------------------------------------------------------------------------
+                Operation                               Time Complexity                         Space Complexity
+         -------------------------------------------------------------------------------------------------------
+         Creating a Singly Linked List                      O(1)                                       O(1)
+         Insertion at 1st position                          O(1)                                       O(1)
+         Insertion at last position                         O(1)                                       O(1)
+         insertion at nth position                          O(N)                                       O(1)
+         Traversing a Singly Linked List                    O(N)                                       O(1)
+         Searching for a value in a Singly Linked List      O(N)                                       O(1)
+         Accessing a value                                  O(N)                                       O(1)
+         Deleting first node                                O(1)                                       O(1)
+         Deleting nth node                                  O(N)                                       O(1)
+         Deleting last node                                 O(N)                                       O(1)
+         Deleting entire Singly Linked List                 O(1)                                       O(1)
     '''
 
 
