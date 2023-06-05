eb396c562dd3ea883745877bb62519ac10f89589
TarakaKoda <taraka.koda@gmail.com>
2023-06-05 22:07:09 +0530

    Time and Space Complexity of Circular Singly Linked List

---
+++ 13 - Linked List/Circular Singly Linked List/06.00 Time and Space of Circular Singly Linked List.py
@@ -0,0 +1,23 @@
+class Linked_List:
+    '''
+         ---------------------------------------------------------------------------------------------------------------
+                Operation                                     Time Complexity                          Space Complexity
+         ---------------------------------------------------------------------------------------------------------------
+         Creating a Circular Singly Linked List                   O(1)                                      O(1)
+         Insertion at 1st position                                O(1)                                      O(1)
+         Insertion at last position                               O(1)                                      O(1)
+         insertion at nth position                                O(N)                                      O(1)
+         Traversing a Circular Singly Linked List                 O(N)                                      O(1)
+         Searching for a value in Circular Singly Linked List     O(N)                                      O(1)
+         Accessing a value                                        O(N)                                      O(1)
+         Deleting first node                                      O(1)                                      O(1)
+         Deleting nth node                                        O(N)                                      O(1)
+         Deleting last node                                       O(N)                                      O(1)
+         Deleting entire Circular Singly Linked List              O(1)                                      O(1)
+    '''
+
+
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
