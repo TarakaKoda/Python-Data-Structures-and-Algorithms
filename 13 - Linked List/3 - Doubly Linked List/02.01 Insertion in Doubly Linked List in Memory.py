e96d677915a284cb8e1fa56abae8b96a19a23fe0
TarakaKoda <taraka.koda@gmail.com>
2023-06-06 21:55:11 +0530

    Insertion in Doubly Linked List in Memory

---
+++ 13 - Linked List/3 - Doubly Linked List/02.01 Insertion in Doubly Linked List in Memory.py
@@ -0,0 +1,13 @@
+class Linked_List:
+    '''
+    Insertion in Doubly Linked List
+    - Inserting into a Doubly Linked List is same as the Singly Linked List but we need to change the references
+      previous and next.
+    - We can insert into Circular Singly Linked List in 3 ways:
+      - At the beginning of the circular singly linked list Time - O(1) space - O(1)
+      - After a node in the middle of circular singly linked list Time - O(n) space - O(1)
+      - At the end of the circular singly linked list Time - O(1) space - O(1)
+    '''
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
