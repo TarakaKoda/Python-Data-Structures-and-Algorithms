ac3bc42ebfe3d7955d6912e54d6b3f5d6d13f0a1
TarakaKoda <taraka.koda@gmail.com>
2023-06-06 21:58:52 +0530

    Time and Space Complexity in Doubly Linked List

---
+++ 13 - Linked List/3 - Doubly Linked List/06.00 Time and Space Complexity in Doubly Linked List.py
@@ -0,0 +1,24 @@
+class Linked_List:
+    '''
+         -------------------------------------------------------------------------------------------------------
+                Operation                               Time Complexity                         Space Complexity
+         -------------------------------------------------------------------------------------------------------
+         Creating a Doubly Linked List                      O(1)                                       O(1)
+         Insertion at 1st position                          O(1)                                       O(1)
+         Insertion at last position                         O(1)                                       O(1)
+         insertion at nth position                          O(N)                                       O(1)
+         Traversing a Doubly Linked List                    O(N)                                       O(1)
+         Reverse Traversing a Doubly Linked List            O(N)                                       O(1)
+         Searching for a value in a Doubly Linked List      O(N)                                       O(1)
+         Accessing a value                                  O(N)                                       O(1)
+         Deleting first node                                O(1)                                       O(1)
+         Deleting nth node                                  O(N)                                       O(1)
+         Deleting last node                                 O(1)                                       O(1)
+         Deleting entire Doubly Linked List                 O(1)                                       O(1)
+    '''
+
+
+
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
