da95c0eb2efd7f7ebf56e8c4805ea9d3fa82ba4f
TarakaKoda <taraka.koda@gmail.com>
2023-06-07 20:01:37 +0530

    Insertion in Circular Doubly Linked List in Memory

---
+++ 13 - Linked List/4 - Circular Doubly Linked List/02.01 Insertion in Circular Doubly Linked List in Memory.py
@@ -0,0 +1,13 @@
+class Linked_List:
+    '''
+    Insertion in Circular Doubly Linked List
+    - Inserting into a Circular Doubly Linked List is same as the Doubly Linked List but we need to change the references
+      previous and next.
+    - We can insert into Circular Doubly Linked List in 3 ways:
+      - At the beginning of the circular singly linked list Time - O(1) space - O(1)
+      - After a node in the middle of circular singly linked list Time - O(n) space - O(1)
+      - At the end of the circular singly linked list Time - O(1) space - O(1)
+    '''
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
