474db75acde0d32a907ffd5bdf0837cf53d1af2b
TarakaKoda <taraka.koda@gmail.com>
2023-06-08 23:54:16 +0530

    List of Linked List Interview Questions

---
+++ 14 - Linked List Interview Questions/00.00 List of Linked List Interview Questions.py
@@ -0,0 +1,12 @@
+class Linked_List:
+    '''
+    1.1 Write a code to remove duplicate from given Linked List
+    1.2 Write a code to remove duplicates without using temporary in a Linked List
+    2.0 Implement and Algorithm to find the nth to last element of a Singly Linked List
+    3.0 Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x.
+    4.0 Write a function that add the two numbers and returns the sum as a linked list
+    5.0 Intersection of two linked list
+    '''
+
+if __name__ == "__main__":
+    print(Linked_List().__doc__)
