419a6bd1dd6e47ffe24587565e01cfb127177959
TarakaKoda <taraka.koda@gmail.com>
2023-06-09 22:27:48 +0530

    finding the intersection of two linked list

--- 14 - Linked List Interview Questions/05.00 Intersection.py
+++ 14 - Linked List Interview Questions/05.00 Intersection.py
@@ -5,37 +5,38 @@ Intersection of two linked list
 from LinkedList import Linked_List, Node
 
 
-def intersection(llA, llB):
-    if llA.tail is not llB.tail:
+def intersection(linked_list1, linked_list2):
+    if linked_list1.tail is not linked_list2.tail:
         return False
 
-    lenA = len(llA)
-    lenB = len(llB)
+    ll1 = len(linked_list1)
+    ll2 = len(linked_list2)
 
-    shorter = llA if lenA < lenB else llB
-    longer = llB if lenA < lenB else llA
+    shorter = linked_list1 if ll1 < ll2 else linked_list2
+    longer = linked_list2 if ll1 < ll2 else linked_list1
 
-    diff = len(longer) - len(shorter)
-    longerNode = longer.head
-    shorterNode = shorter.head
+    difference = len(longer) - len(shorter)
 
-    for i in range(diff):
-        longerNode = longerNode.next
+    longer_node = longer.head
+    shorter_node = shorter.head
 
-    while shorterNode is not longerNode:
-        shorterNode = shorterNode.next
-        longerNode = longerNode.next
+    for _ in range(difference):
+        longer_node = longer_node.next
 
-    return longerNode
+    while shorter_node is not longer_node:
+        shorter_node = shorter_node.next
+        longer_node = longer_node.next
+    return longer_node
+
 
 
 # Helper addition method
-def addSameNode(llA, llB, value):
-    tempNode = Node(value)
-    llA.tail.next = tempNode
-    llA.tail = tempNode
-    llB.tail.next = tempNode
-    llB.tail = tempNode
+def adding_insertion_between_linked_list(linked_list1, linked_list2, value):
+    new_node = Node(value)
+    linked_list1.tail.next = new_node
+    linked_list1.tail = new_node
+    linked_list2.tail.next = new_node
+    linked_list2.tail = new_node
 
 
 llA = Linked_List()
@@ -44,8 +45,8 @@ llA.generate(3, 10, 0)
 llB = Linked_List()
 llB.generate(4, 10, 0)
 
-addSameNode(llA, llB, 11)
-addSameNode(llA, llB, 14)
+adding_insertion_between_linked_list(llA, llB, 11)
+adding_insertion_between_linked_list(llA, llB, 14)
 
 if __name__ == "__main__":
     print(__doc__)
