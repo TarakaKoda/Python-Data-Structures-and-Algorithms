a96f3c75ded709959e690bbadc8dc557ece9a027
TarakaKoda <taraka.koda@gmail.com>
2023-06-09 22:26:03 +0530

    Updated code to partition a linked list around a value

--- 14 - Linked List Interview Questions/03.00 Partition.py
+++ 14 - Linked List Interview Questions/03.00 Partition.py
@@ -5,24 +5,26 @@ Write code to partition a linked list around a value x, such that all nodes less
 from LinkedList import Linked_List
 
 
-def partition(ll, x):
-    curNode = ll.head
-    ll.tail = ll.head
+def partition(linked_list, value):
+    if linked_list.head is None:
+        return None
+    else:
+        current_node = linked_list.head
+        linked_list.tail = linked_list.head
 
-    while curNode:
-        nextNode = curNode.next
-        curNode.next = None
-        if curNode.value < x:
-            curNode.next = ll.head
-            ll.head = curNode
-        else:
-            ll.tail.next = curNode
-            ll.tail = curNode
-        curNode = nextNode
-
-    if ll.tail.next is not None:
-        ll.tail.next = None
-
+        while current_node:
+            next_node = current_node.next
+            current_node.next = None
+            if current_node.value < value:
+                current_node.next = linked_list.head
+                linked_list.head = current_node
+            else:
+                linked_list.tail.next = current_node
+                linked_list.tail = current_node
+            current_node = next_node
+        if linked_list.tail.next:
+            linked_list.tail.next = None
+        return linked_list
 
 customLL = Linked_List()
 customLL.generate(10, 99, 0)
