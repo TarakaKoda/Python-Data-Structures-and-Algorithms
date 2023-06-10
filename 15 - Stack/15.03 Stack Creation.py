01972696a78fe1c1b2daf7979abab00b7d630848
TarakaKoda <taraka.koda@gmail.com>
2023-06-10 20:22:01 +0530

    Stack Creation

---
+++ 15 - Stack/15.03 Stack Creation.py
@@ -0,0 +1,23 @@
+class Stack:
+    '''
+    STACK CREATION:
+    There are various ways of implementing Stack in python.
+
+    Now let see 3 cases:
+    1. using a python list without limit
+    2. using a python list with limit
+    3. using a python linked list
+
+    Difference between LIST vs LINKED LIST as STACK
+
+    Stack using List
+    - easy to implement
+    - speed problem when it grows
+
+    Stack using Linked List
+    - fast performance
+    - implementation is that easy
+    '''
+
+if __name__ == "__main__":
+    print(Stack().__doc__)
