81e3e904f5b9151fb21ad06cd4393445ac7fe42f
TarakaKoda <taraka.koda@gmail.com>
2023-06-10 20:26:40 +0530

    When to USE/AVOID Stack

---
+++ 15 - Stack/15.12 When to Use and Avoid Stack.py
@@ -0,0 +1,12 @@
+class Stack:
+    '''
+    When to use/avoid stack
+      use
+      - When you want to use LIFO functionality
+      - The chance of data corruption is minimum
+      Avoid
+      - Random access is not possible
+    '''
+
+if __name__ == "__main__":
+    print(Stack().__doc__)
