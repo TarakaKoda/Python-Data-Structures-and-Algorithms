92384fa91568d5d1e6bb0703b12da60cdc7604c3
TarakaKoda <taraka.koda@gmail.com>
2023-06-10 20:25:22 +0530

    Creating Stack using Linked List

---
+++ 15 - Stack/15.09 Creating Stack using Linked List.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class Linked_List:
+    def __init__(self):
+        self.head = None
+
+class Stack:
+    def __init__(self):
+        self.linked_list = Linked_List()
+
+
