ed488d30624e0d145b339ffd664d7ff79bd82db4
TarakaKoda <taraka.koda@gmail.com>
2023-06-10 20:21:21 +0530

    Introduction to Stack

---
+++ 15 - Stack/15.01 What is a Stack.py
@@ -0,0 +1,34 @@
+class Stack:
+    '''
+    What is a stack?
+    Stack is a Data Structure that stores items in a LAST IN FIRST OUT manner.
+
+    - Creating Stacks using Python lists and linked lists
+    - Operations(push,pop,peek,isEmpty,isFull)
+
+    Why do We need it?
+    We need a Stack Data Structure when we need to utilize the last coming data first
+
+    Visual representation of stack:
+
+    1 -> 2 -> 3 -> 4 -> 5 -> 6
+
+    LAST IN FIRST OUT : Here 1 is inserted first and 6 is inserted last
+
+    +---+
+    | 6 |
+    +---+
+    | 5 |
+    +---+
+    | 4 |
+    +---+
+    | 3 |
+    +---+
+    | 2 |
+    +---+
+    | 1 |
+    +---+
+    '''
+
+if __name__ == "__main__":
+    print(Stack().__doc__)
