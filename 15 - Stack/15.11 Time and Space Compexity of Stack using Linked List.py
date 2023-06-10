03fd9a8fa049def8bf7c325e6a5bf565c6f46c66
TarakaKoda <taraka.koda@gmail.com>
2023-06-10 20:26:12 +0530

    Time and Space Complexity of Stack using Linked List

---
+++ 15 - Stack/15.11 Time and Space Compexity of Stack using Linked List.py
@@ -0,0 +1,16 @@
+class Stack:
+    '''
+         -----------------------------------------------------------------------------------------------
+                Operation                     Time Complexity                         Space Complexity
+         -----------------------------------------------------------------------------------------------
+         Create a Stack                            O(1)                                       O(1)
+         Push                                      O(1)                                       O(1)
+         Pop                                       O(1)                                       O(1)
+         Peek                                      O(1)                                       O(1)
+         is_empty                                  O(1)                                       O(1)
+         Delete Entire Stack                       O(1)                                       O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(Stack().__doc__)
