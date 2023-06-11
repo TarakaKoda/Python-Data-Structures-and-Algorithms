be90f8377b22714083db7ffe5f0aa00de009be50
TarakaKoda <taraka.koda@gmail.com>
2023-06-11 18:44:19 +0530

    Queue Creation

---
+++ 16 - Queue/16.03 Queue Creation.py
@@ -0,0 +1,24 @@
+class Queue:
+    '''
+    QUEUE CREATION:
+    There are various ways of implementing Queue in python.
+
+    Now let see 3 cases:
+    1. using a python list without limit
+    2. using a python list with limit
+    3. using a python linked list
+
+    Difference between LIST vs LINKED LIST as QUEUE
+
+    Queue using List
+    - easy to implement
+    - with size limit list are more efficient comparing to list with without limit
+    - speed problem when it grows in the case of list without limit
+
+    Queue using Linked List
+    - fast performance
+    - implementation is that hard
+    '''
+
+if __name__ == "__main__":
+    print(Queue().__doc__)
