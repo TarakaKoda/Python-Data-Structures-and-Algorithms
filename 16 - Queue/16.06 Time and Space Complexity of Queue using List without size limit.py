1d4566a81aca2e26271008d556d8a15b5197b06f
TarakaKoda <taraka.koda@gmail.com>
2023-06-11 18:46:44 +0530

    Time and Space Complexity of Queue using List without size limit

---
+++ 16 - Queue/16.06 Time and Space Complexity of Queue using List without size limit.py
@@ -0,0 +1,16 @@
+class Queue:
+    '''
+         -----------------------------------------------------------------------------------------------
+                Operation                     Time Complexity                         Space Complexity
+         -----------------------------------------------------------------------------------------------
+         Create a Queue                            O(1)                                       O(1)
+         Enqueue                                   O(N)                                       O(1)
+         Dequeue                                   O(N)                                       O(1)
+         Peek                                      O(1)                                       O(1)
+         is_empty                                  O(1)                                       O(1)
+         Delete Entire Queue                       O(1)                                       O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(Queue().__doc__)
