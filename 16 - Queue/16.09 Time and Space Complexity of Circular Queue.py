5ca56f6be18632ff8ac60050fe2255c50580cb4a
TarakaKoda <taraka.koda@gmail.com>
2023-06-11 18:48:54 +0530

    Time and Space Complexity of Circular Queue

---
+++ 16 - Queue/16.09 Time and Space Complexity of Circular Queue.py
@@ -0,0 +1,17 @@
+class Queue:
+    '''
+         -----------------------------------------------------------------------------------------------
+            Operation                        Time Complexity                         Space Complexity
+         -----------------------------------------------------------------------------------------------
+         Create a Queue                            O(1)                                       O(N)
+         Enqueue                                   O(1)                                       O(1)
+         Dequeue                                   O(1)                                       O(1)
+         Peek                                      O(1)                                       O(1)
+         is_empty                                  O(1)                                       O(1)
+         is_full                                   O(1)                                       O(1)
+         Delete Entire Queue                       O(1)                                       O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(Queue().__doc__)
