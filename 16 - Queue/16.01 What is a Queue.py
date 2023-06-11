3cafa35dbcad5d203b68228db3a03717c67b91c7
TarakaKoda <taraka.koda@gmail.com>
2023-06-11 18:42:25 +0530

    Introduction to Queue

---
+++ 16 - Queue/16.01 What is a Queue.py
@@ -0,0 +1,23 @@
+class Queue:
+    '''
+    What is a queue?
+    Queue is a Data Structure that stores items in a FIRST IN FIRST OUT manner.
+
+    - Creating Stacks using Python lists and linked lists
+    - Operations(enqueue,dequeue,peek,isEmpty,isFull)
+
+    Why do We need it?
+    We need a Stack Data Structure when we need to utilize the first coming data first
+
+    Visual representation of stack:
+
+    1 <- 2 <- 3 <- 4 <- 5 <- 6
+
+    FIRST IN FIRST OUT : Here 1 is inserted first and 6 is inserted last
+
+    [1 <- 2 <- 3 <- 4 <- 5 <- 6]
+    '''
+
+
+if __name__ == "__main__":
+    print(Queue().__doc__)
