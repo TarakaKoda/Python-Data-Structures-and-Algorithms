498463eb8c3f0be5137f3d969e769a81d4cbd291
TarakaKoda <taraka.koda@gmail.com>
2023-06-13 21:43:28 +0530

    Create Stack with minimum method

--- 17 - Stack and Queue Interview Questions/02 Stack Minimum.py
+++ 17 - Stack and Queue Interview Questions/02 Stack Minimum.py
@@ -1,51 +1,49 @@
 #   Create Stack with min method
 
-class Node():
+class Node:
     def __init__(self, value=None, next=None):
         self.value = value
         self.next = next
-
     def __str__(self):
-        string = str(self.value)
-        if self.next:
-            string += ',' + str(self.next)
+        string = self.value
+        string += "," + str(self.next)
         return string
 
-
-class Stack():
+class Stack:
     def __init__(self):
         self.top = None
-        self.minNode = None
+        self.min_node = None
 
     def min(self):
-        if not self.minNode:
+        if self.min_node is None:
             return None
-        return self.minNode.value
-
-    def push(self, item):
-        if self.minNode and (self.minNode.value < item):
-            self.minNode = Node(value=self.minNode.value, next=self.minNode)
         else:
-            self.minNode = Node(value=item, next=self.minNode)
-        self.top = Node(value=item, next=self.top)
+            return self.min_node.value
+
+    def push(self, value):
+        if self.min_node and (self.min_node.value < value):
+            self.min_node = Node(value=self.min_node.value, next=self.min_node)
+        else:
+            self.min_node = Node(value=value, next=self.min_node)
+        self.top = Node(value=value, next=self.top)
 
     def pop(self):
-        if not self.top:
-            return None
-        self.minNode = self.minNode.next
-        item = self.top.value
-        self.top = self.top.next
-        return item
+        if self.top is None:
+            return "Stack is empty"
+        else:
+            self.min_node = self.min_node.next
+            top_value = self.top
+            self.top = self.top.next
+            return top_value
 
 
-customStack = Stack()
+stack = Stack()
 
-if __name__ == "__main__":
-    customStack.push(5)
-    print(customStack.min())
-    customStack.push(6)
-    print(customStack.min())
-    customStack.push(3)
-    print(customStack.min())
-    customStack.pop()
-    print(customStack.min())
+if __name__  == "__main__":
+    print(stack.min())
+    stack.push(5)
+    stack.push(3)
+    stack.push(2)
+    print(stack.min())
+    stack.pop()
+    print(stack.min())
