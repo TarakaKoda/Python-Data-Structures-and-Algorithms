aec23ddeeaffa729c31b5e602b49cbcd328f92b4
TarakaKoda <taraka.koda@gmail.com>
2023-06-13 21:44:28 +0530

    Stack of Plates problem

--- 17 - Stack and Queue Interview Questions/03 Stack Plates.py
+++ 17 - Stack and Queue Interview Questions/03 Stack Plates.py
@@ -1,18 +1,17 @@
 # Stack of Plates
-
-class Stack_Plate():
+class Stack_Plates:
     def __init__(self, capacity):
         self.capacity = capacity
         self.stacks = []
 
     def __str__(self):
-        return self.stacks
+        return str(self.stacks)
 
-    def push(self, item):
-        if len(self.stacks) > 0 and (len(self.stacks[-1])) < self.capacity:
-            self.stacks[-1].append(item)
+    def push(self, value):
+        if len(self.stacks) > 0 and (len(self.stacks[-1]) < self.capacity):
+            self.stacks[-1].append(value)
         else:
-            self.stacks.append([item])
+            self.stacks.append([value])
 
     def pop(self):
         while len(self.stacks) and len(self.stacks[-1]) == 0:
@@ -22,18 +21,24 @@ class Stack_Plate():
         else:
             return self.stacks[-1].pop()
 
-    def pop_at(self, stackNumber):
-        if len(self.stacks[stackNumber]) > 0:
-            return self.stacks[stackNumber].pop()
-        else:
-            return None
+    def pop_at(self, stack_number):
+        if len(self.stacks) > stack_number:
+            if len(self.stacks[stack_number]) > 0:
+                return self.stacks[stack_number].pop()
+            else:
+                return None
 
-
-customStack = Stack_Plate(2)
+stack = Stack_Plates(3)
 
 if __name__ == "__main__":
-    customStack.push(1)
-    customStack.push(2)
-    customStack.push(3)
-    customStack.push(4)
-    print(customStack.pop_at(1))
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+    print(stack)
+    stack.pop()
+    print(stack)
+    stack.pop()
+    print(stack)
+    print(stack.pop_at(1))
+    print(stack)
