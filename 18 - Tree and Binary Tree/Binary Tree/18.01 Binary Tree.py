b90151f9d5daabde470507a7bd689338c19efee7
TarakaKoda <taraka.koda@gmail.com>
2023-06-14 23:50:39 +0530

    Introduction to Binary Tree

---
+++ 18 - Tree and Binary Tree/Binary Tree/18.01 Binary Tree.py
@@ -0,0 +1,14 @@
+class Binary_Tree:
+    '''
+      Binary Tree
+      - Binary trees are the data structures in which each node has at most two children, often referred to as the left and right children
+      - Binary is a family data structure(BST,Head tree,AVL,red black tree,Syntax tree)
+
+      Why binary tree?
+      - Binary trees are a prerequisite for more advanced trees like BST, AVL, Red Black Trees
+      - Huffman coding problem, heap priority problem and expression parsing problem can be solved efficiently using binary trees
+    '''
+
+if __name__ == "__main__":
+    print(Binary_Tree().__doc__)
+
