5bb3d0a6079f50293453160bb94dee2606b7ac92
TarakaKoda <taraka.koda@gmail.com>
2023-06-14 23:49:11 +0530

    Introduction to Tree

---
+++ 18 - Tree and Binary Tree/18.01 What is a Tree.py
@@ -0,0 +1,17 @@
+class Tree:
+    '''
+    What is a Tree?
+    - Tree is a non-linear data structure with hierarchical relationships with elements without having any cycles,
+      its basically reverse from a real life tree
+
+    Properties:
+    - Represent Hierarchical data.
+    - Each node has two components data and link.
+       - Data: it's the value that store in it.
+       - Link: it's the reference to the next corresponding sub category
+    - Base category and sub category under it.
+    '''
+
+
+if __name__ == "__main__":
+    print(Tree().__doc__)
