81159dbf0e93b295ebfb9728b003222ffb5793e6
TarakaKoda <taraka.koda@gmail.com>
2023-06-14 23:50:09 +0530

    Tree Terminologies

---
+++ 18 - Tree and Binary Tree/18.03 Tree Terminology.py
@@ -0,0 +1,24 @@
+class Tree:
+    '''
+    Root: Top node without parent.
+
+    Edge: A link between parent and child
+
+    Leaf: A node which does not have children
+
+    Sibling: Children with same parent
+
+    Ancestor: Parent, grandparent, and great-grandparent of a node
+
+    Depth of a node: Length of the path from root node to the node
+
+    Height of node: Length of the path from node to the deepest node
+
+    Depth of tree: depth of root
+
+    height of tree: height of root
+    '''
+
+
+if __name__ == "__main__":
+    print(Tree().__doc__)
