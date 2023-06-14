e961d017a8250dd3e8bbe4219fc3a7d83210fce6
TarakaKoda <taraka.koda@gmail.com>
2023-06-14 23:49:40 +0530

    Why we need a Tree

---
+++ 18 - Tree and Binary Tree/18.02 Why we need a Tree.py
@@ -0,0 +1,18 @@
+class Tree:
+    '''
+    Why a Tree?
+    - Quicker and easier to access to the data
+    - Store hierarchical data, like folder structure, organize structure, XML,HTML data
+    - There are many different types of data structure which performs better in various situations
+        - Binary Search Tree, AVL, Red Black Tree, Trie
+    '''
+
+
+if __name__ == "__main__":
+    print(Tree().__doc__)
+
+
+
+if __name__ == "__main__":
+    print(Tree().__doc__)
+
