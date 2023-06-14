13a5f9b02d346dc444ef57c5d877b3cf956603c0
TarakaKoda <taraka.koda@gmail.com>
2023-06-14 23:51:01 +0530

    Types of Binary Tree

---
+++ 18 - Tree and Binary Tree/Binary Tree/18.02 Types of Binary Tree.py
@@ -0,0 +1,11 @@
+class Binary_Tree:
+    '''
+      Types of Binary trees
+      - Full binary tree : either 2 or 0 children
+      - perfect binary tree : 2 children and have same level
+      - complete binary tree
+      - Balanced binary tree - all leafs have same depth
+    '''
+
+if __name__ == "__main__":
+    print(Binary_Tree().__doc__)
