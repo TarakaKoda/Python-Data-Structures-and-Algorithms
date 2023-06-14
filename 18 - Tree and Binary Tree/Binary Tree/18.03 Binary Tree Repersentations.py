9756d47a5808bff559526fcaec79fb93e47e5f25
TarakaKoda <taraka.koda@gmail.com>
2023-06-14 23:51:22 +0530

    Binary Tree Representations

---
+++ 18 - Tree and Binary Tree/Binary Tree/18.03 Binary Tree Repersentations.py
@@ -0,0 +1,9 @@
+class Binary_Tree:
+    '''
+     How to create basic binary tree in python?
+      - Linked list - data, left-child ref, right-child ref
+      - Python list(array) - left child = cell[2X] right child = cell[2x+2]
+    '''
+
+if __name__ == "__main__":
+    print(Binary_Tree().__doc__)
