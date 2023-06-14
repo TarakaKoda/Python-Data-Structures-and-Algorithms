3fc50c4a00829912083d7e46da3bc7c7233c2768
TarakaKoda <taraka.koda@gmail.com>
2023-06-14 23:53:39 +0530

    Creating a Binary Tree using Linked List

---
+++ 18 - Tree and Binary Tree/Binary Tree/01 Binary Tree using Linked List/18.01.01 Create a Binary Tree using Linked List.py
@@ -0,0 +1,9 @@
+class Binary_Tree:
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+
+if __name__ == "__main__":
+    binary_tree = Binary_Tree("drink")
