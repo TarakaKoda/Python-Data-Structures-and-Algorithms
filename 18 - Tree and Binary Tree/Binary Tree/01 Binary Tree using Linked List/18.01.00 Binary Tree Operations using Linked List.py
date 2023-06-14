b4c2b172374cdaf6be1fdf6ad5d70cf25631a5b2
TarakaKoda <taraka.koda@gmail.com>
2023-06-14 23:53:07 +0530

    Binary Tree Operations using Linked List

---
+++ 18 - Tree and Binary Tree/Binary Tree/01 Binary Tree using Linked List/18.01.00 Binary Tree Operations using Linked List.py
@@ -0,0 +1,29 @@
+class Binary_Tree:
+    '''
+    Binary Tree Operations:
+    1. Creation of Tree
+
+    2. Insertion of a node
+
+    3. Deletion of node
+
+    4. searching for a value
+       DFS and BFS are the Algorithms used for Searching
+       - Depth First Search (DFS)
+         - PreOrder Traversal Binary Tree
+         - InOrder Traversal Binary Tree
+         - PostOrder Traversal Binary Tree
+       - Breath First Search (BFS)
+         - LevelOrder Traversal Binary Tree
+
+    5. Traverse all nodes
+         - PreOrder Traversal Binary Tree
+         - InOrder Traversal Binary Tree
+         - PostOrder Traversal Binary Tree
+         - LevelOrder Traversal Binary Tree
+
+    6. Deletion of Tree
+    '''
+
+if __name__ == "__main__":
+    print(Binary_Tree().__doc__)
