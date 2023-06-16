e879f35e841c581e50dadee775380626d5f4c32c
TarakaKoda <taraka.koda@gmail.com>
2023-06-16 23:54:57 +0530

    Putting learning into practice: A comprehensive exercises

--- Practice.py
+++ Practice.py
@@ -2315,6 +2315,26 @@ class Binary_Tree:
         self.last_used_index = 0
         self.maximum_size = size
 
-new_tree = Binary_Tree(8)
+    def insert(self, value):
+        if self.last_used_index+1 == self.maximum_size:
+            return "Tree is full"
+        else:
+            self.list[self.last_used_index+1] = value
+            self.last_used_index += 1
+            return "Successfully inserted"
+
+    def __str__(self):
+        values = [str(value) for value in self.list]
+        return "\n".join(values)
+
+binary_tree = Binary_Tree(5)
+
+binary_tree.insert("Drinks")
+binary_tree.insert("Hot")
+binary_tree.insert("Cold")
+binary_tree.insert("Tea")
+
+if __name__ == "__main__":
+    print(binary_tree)
 
 
