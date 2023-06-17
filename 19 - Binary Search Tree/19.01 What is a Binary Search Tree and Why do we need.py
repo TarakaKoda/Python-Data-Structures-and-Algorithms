0eabdda84d74b314d55b3c8a7fd12911a923f5a6
TarakaKoda <taraka.koda@gmail.com>
2023-06-17 23:07:19 +0530

    Introduction to Binary Search Tree

---
+++ 19 - Binary Search Tree/19.01 What is a Binary Search Tree and Why do we need.py
@@ -0,0 +1,17 @@
+class Binary_Search_Tree:
+    '''
+      What is BST?
+      - In the left subtree the value of a node is less than or equal to its parent node's value
+      - In the right subtree the value of a node is greater than its parent node's value
+
+      Common operations on binary search tree
+      - creation of tree
+      - insertion of a node
+      - deletion of a node
+      - search for a node
+      - traversal all nodes
+    '''
+
+
+if __name__ == "__main__":
+    print(Binary_Search_Tree().__doc__)
