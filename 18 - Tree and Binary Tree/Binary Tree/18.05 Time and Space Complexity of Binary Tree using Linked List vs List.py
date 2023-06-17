7e23c05c5bdc0eec6640bf2b0a83dc2c88675ff5
TarakaKoda <taraka.koda@gmail.com>
2023-06-17 13:05:47 +0530

    Time and Space Complexity of a Binary Tree using Linked List vs List

---
+++ 18 - Tree and Binary Tree/Binary Tree/18.05 Time and Space Complexity of Binary Tree using Linked List vs List.py
@@ -0,0 +1,18 @@
+class Binary_Tree:
+    '''
+ -----------------------------------------------------------------------------------------------------------------------
+                                                        List                                  Linked List
+ -----------------------------------------------------------------------------------------------------------------------
+    Operation                           Time Complexity  Space Complexity            Time Complexity  Space Complexity
+ -----------------------------------------------------------------------------------------------------------------------
+ Create a Binary Tree                        O(1)              O(N)                        O(1)             O(1)
+ Inserting node to Binary Tree               O(1)              O(1)                        O(N)             O(N)
+ Deleting node from Binary Tree              O(N)              O(1)                        O(N)             O(N)
+ Searching for a node in Binary Tree         O(N)              O(1)                        O(N)             O(N)
+ Traverse a Binary Tree                      O(N)              O(1)                        O(N)             O(N)
+ Delete entire Binary Tree                   O(1)              O(1)                        O(1)             O(1)
+ Space efficient?                                               NO                                          YES
+'''
+
+if __name__ == "__main__":
+    print(Binary_Tree().__doc__)
