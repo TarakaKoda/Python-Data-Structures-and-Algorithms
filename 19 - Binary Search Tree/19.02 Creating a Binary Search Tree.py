0a38cb8b4e4bc9bc52190e0778466f2e88b94211
TarakaKoda <taraka.koda@gmail.com>
2023-06-17 23:07:43 +0530

    Creating a Binary Search Tree

---
+++ 19 - Binary Search Tree/19.02 Creating a Binary Search Tree.py
@@ -0,0 +1,7 @@
+class Binary_Search_Tree:
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child =None
+
+binary_search_tree = Binary_Search_Tree(57)
