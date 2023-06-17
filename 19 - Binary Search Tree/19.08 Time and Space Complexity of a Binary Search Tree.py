6cbbc8815f50fb424d20f93dd0930b628d5f71c0
TarakaKoda <taraka.koda@gmail.com>
2023-06-17 23:11:16 +0530

    Time and Space Complexity of a Binary Search Tree

---
+++ 19 - Binary Search Tree/19.08 Time and Space Complexity of a Binary Search Tree.py
@@ -0,0 +1,16 @@
+class Binary_Search_Tree:
+    '''
+         -------------------------------------------------------------------------------------------------------
+                Operation                              Time Complexity                         Space Complexity
+         -------------------------------------------------------------------------------------------------------
+         Creating Binary Search Tree                        O(1)                                       O(1)
+         Inserting values in Binary Search Tree             O(LogN)                                    O(LogN)
+         Traversing though a Binary Search Tree             O(N)                                       O(N)
+         Searching for a value in Binary Search Tree        O(LogN)                                    O(LogN)
+         Deleting a node from Binary Search Tree            O(LogN)                                    O(LogN)
+         Delete Entire Binary Search Tree                   O(1)                                       O(1)
+    '''
+
+
+if __name__ == "__main__":
+    print(Binary_Search_Tree().__doc__)
