078569e0b800c1ec8041d373cf266293eaca6111
TarakaKoda <taraka.koda@gmail.com>
2023-06-18 23:35:16 +0530

    Why do wee need AVL Tree

---
+++ 20 - AVL (Adelson-Velsky and Landis) Tree/20.02 Why do we need AVL Tree.py
@@ -0,0 +1,11 @@
+class AVL_Tree:
+    '''
+    why do we need AVL tree?
+    - If there are more numbers greater than root the  tree height becomes very long and searching and other operations
+      are performed lower than expected so to overcome this difficulty AVL comes into picture.
+
+    - AVL trees are essential because they provide a balanced tree structure, improved time complexity, guaranteed
+      worst-case performance, and a versatile data structure for efficient search, insertion, and deletion operations.
+      They are an effective solution when maintaining a balanced binary search tree is necessary for optimal performance
+      in a wide range of applications.
+    '''
