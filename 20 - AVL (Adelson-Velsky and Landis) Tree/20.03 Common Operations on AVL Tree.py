e3bd5d5cf18d362ec2341159a1716a30ba5e77a5
TarakaKoda <taraka.koda@gmail.com>
2023-06-18 23:35:38 +0530

    Common Operations on AVL Tree

---
+++ 20 - AVL (Adelson-Velsky and Landis) Tree/20.03 Common Operations on AVL Tree.py
@@ -0,0 +1,22 @@
+class AVL_Tree:
+    '''
+    AVL Tree Operations:
+    1. Creation of AVL Tree
+
+    2. Traverse all nodes
+     - PreOrder Traversal AVL Tree
+     - InOrder Traversal AVL Tree
+     - PostOrder Traversal AVL Tree
+     - LevelOrder Traversal AVL Tree
+
+    3. searching for a value in AVL Tree
+
+    4. Insertion of a node into AVL Tree
+    
+    5. Deletion of a node in AVL Tree
+    
+    6. Deletion of entire AVL Tree
+    '''
+
+if __name__ == "__main__":
+    print(AVL_Tree().__doc__)
