d0ca3c84f724a1fe5c558d42b144d9ae9c5098b9
TarakaKoda <taraka.koda@gmail.com>
2023-06-19 23:51:04 +0530

    Common Operations on Binary Heap

---
+++ 21 - Binary Heap/21.03 Common Opertions on Binary Heap.py
@@ -0,0 +1,18 @@
+class Binary_Heap:
+    '''
+    Common operations on binary heap
+      - Creation of binary heap
+      - peek top of binary heap
+      - Extract Min ? Extract Max
+      - Traversal of binary heap
+      - Size of binary heap
+      - Insert value in binary heap
+      - Delete the entire binary heap
+
+    Implementation options
+      - Array Implementation
+      - Reference/Pointer Implementation
+    '''
+
+if __name__ == "__main__":
+    print(Binary_Heap().__doc__)
