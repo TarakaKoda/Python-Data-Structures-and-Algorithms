65c6f5e611c402324e278dd41bf9ad6faff653a6
TarakaKoda <taraka.koda@gmail.com>
2023-06-19 23:49:06 +0530

    Introduction to Binary Heap

---
+++ 21 - Binary Heap/21.01 What is Binary Heap.py
@@ -0,0 +1,17 @@
+class Binary_Head:
+    '''
+    What is Binary Heap?
+      Creation of Binary Heap
+      Common operations on Binary Heap
+      Insertion in Binary Heap
+      Extraction from Binary Heap
+
+      What is a Binary Heap
+      A Binary Heap is a Binary tree with following properties
+      - A binary head is either Min Heap or Max Heap. In a Min Binary Heap, the key at the root must be minimum among all keys present in Binary Heap. The same property must be recursively true for all nodes in Binart tree
+      - It's a complete tree(All levels are completely filled except possibly the last level and the last level has all keys as left as possible). This property of Binary Heap makes them suitable to be stored in an array
+
+    '''
+    
+if __name__ == "__main__":
+    print(Binary_Head().__doc__)
