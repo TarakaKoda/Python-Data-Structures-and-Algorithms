ae2198cc90a86a6a6abb2091465aa2baf8b4d507
TarakaKoda <taraka.koda@gmail.com>
2023-06-19 23:51:41 +0530

    Binary Heap Operations

---
+++ 21 - Binary Heap/21.04 Binary Heap Operatons.py
@@ -0,0 +1,31 @@
+class Binary_Heap:
+    '''
+     Creation of Binary heap
+
+      initializer fixed sized list
+      set size of binary heap to 0
+      Time O(1) Space - O(n)
+
+      peek of binary heap
+      return list[1]
+      Time O(1) Space - O(1)
+
+      Size of binary heap
+      Return number if filled cells
+      Time O(1) Space - O(1)
+
+      Traversal of binary heap
+      - pre order Time O(n) Space - O(n)
+      - in order Time O(n) Space - O(n)
+      - post order Time O(n) Space - O(n)
+      - level order - Time O(n) space O(1)
+
+      Insert a node in Binary Heap
+      Time O(logN) space - O(logN)
+
+      Extract a node from Binary Heap
+      Time O(logN) space - O(logN)
+
+      Delete entire Binary Heap
+      Time O(1) Space - O(1)
+    '''
