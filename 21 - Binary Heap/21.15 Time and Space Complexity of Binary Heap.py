5d46c7b0d667db895b8ebf75d5dfdcbe0e730b6e
TarakaKoda <taraka.koda@gmail.com>
2023-06-20 23:46:35 +0530

    Time and Space Complexity of Binary Heap

---
+++ 21 - Binary Heap/21.15 Time and Space Complexity of Binary Heap.py
@@ -0,0 +1,16 @@
+class Binary_Heap:
+    '''
+         -------------------------------------------------------------------------------------------------------
+                Operation                              Time Complexity                         Space Complexity
+         -------------------------------------------------------------------------------------------------------
+         Creating Binary Heap                        O(1)                                       O(N)
+         Peek of Heap                                O(1)                                       O(1)
+         Size of Heap                                O(1)                                       O(1)
+         Traversing though a Binary Heap             O(N)                                       O(1)
+         Inserting values in Binary Heap             O(logN)                                    O(logN)
+         Deleting a node from Binary Heap            O(LogN)                                    O(LogN)
+         Delete Entire Binary Heap                   O(1)                                       O(1)
+    '''
+
+if __name__ == "__main__":
+    print(Binary_Heap().__doc__)
