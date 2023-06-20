93e68a13e7ba91a557b13c3b1d37c4da6bba4df9
TarakaKoda <taraka.koda@gmail.com>
2023-06-20 15:22:12 +0530

    Time and Space Complexity of AVL Tree

---
+++ 20 - AVL (Adelson-Velsky and Landis) Tree/20.16 Time and Space Compleity of AVL Tree.py
@@ -0,0 +1,15 @@
+class AVL_Tree:
+    '''
+         -------------------------------------------------------------------------------------------------------
+                Operation                              Time Complexity                         Space Complexity
+         -------------------------------------------------------------------------------------------------------
+         Creating AVL Tree                        O(1)                                       O(1)
+         Inserting values in AVL Tree             O(LogN)                                    O(LogN)
+         Traversing though a AVL Tree             O(N)                                       O(N)
+         Searching for a value in AVL Tree        O(LogN)                                    O(LogN)
+         Deleting a node from AVL Tree            O(LogN)                                    O(LogN)
+         Delete Entire AVL Tree                   O(1)                                       O(1)
+    '''
+
+if __name__ == "__main__":
+    print(AVL_Tree().__doc__)
