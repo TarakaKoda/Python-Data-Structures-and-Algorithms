1ac7f24c36c44c031c58e0b5101863813d8a48c5
TarakaKoda <taraka.koda@gmail.com>
2023-06-21 23:55:42 +0530

    Hash Table is Full

---
+++ 23 - Hashing/23.05 Hash Table is Full.py
@@ -0,0 +1,11 @@
+class Hashing:
+    '''
+    Hash table is full
+    - Direct chaining - This situation never arise
+    - Open addressing - create 2x size of current hash table and recall hashing for current keys
+
+    '''
+
+
+if __name__ == "__main__":
+    print(Hashing().__doc__)
