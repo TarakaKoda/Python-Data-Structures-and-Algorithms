6676cccc899a6f30f7221c3a31e5bdeea22c389e
TarakaKoda <taraka.koda@gmail.com>
2023-06-21 20:26:42 +0530

    Introduction to Trie

---
+++ 22 - Trie/22.01 What is Trie.py
@@ -0,0 +1,14 @@
+class Trie:
+    '''
+    What is a trie?
+    A trie is a tree-based data structure that organizes information in hierarchy
+    properties
+    - It is typically used to store or search strings in a space and time efficient way.
+    - Any node in trie can store non-repetitive multiple characters
+    - Every node stores link og the next character of the string
+    - Every node keep track of 'end of string'
+    '''
+
+if __name__ == "__main__":
+    print(Trie().__doc__)
+
