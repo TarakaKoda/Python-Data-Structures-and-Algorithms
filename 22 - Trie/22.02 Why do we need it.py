d8613a401f9fe19b73decfe7b6a6135128eabdab
TarakaKoda <taraka.koda@gmail.com>
2023-06-21 20:27:03 +0530

    Why do we need Trie

---
+++ 22 - Trie/22.02 Why do we need it.py
@@ -0,0 +1,18 @@
+class Trie:
+    '''
+    Why we need trie?
+    To solve many standard problems in efficient way
+    - spell checker
+    - Auto completion
+
+                Map
+      Characters | Link to trie node
+                 |
+                 |
+                 |
+           End of string
+    '''
+
+
+if __name__ == "__main__":
+    print(Trie().__doc__)
