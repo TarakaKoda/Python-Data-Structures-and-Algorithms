a8546d41e70cf9ce52e893f031ade86dc942127a
TarakaKoda <taraka.koda@gmail.com>
2023-06-21 20:34:06 +0530

    Creating a Trie data structure

---
+++ 22 - Trie/Trie Practice/01 Creating a Trie.py
@@ -0,0 +1,18 @@
+class Trie_Node:
+    def __init__(self):
+        self.children = {}
+        self.end_of_node = False
+
+
+class Trie:
+    def __inti__(self):
+        self.root_node = Trie_Node()
+
+
+
+
+if __name__ == "__main__":
+    new_trie = Trie()
+
+
+
