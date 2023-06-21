9b111cb45bd0c20a7f5144d2756ed8be7ecf14ad
TarakaKoda <taraka.koda@gmail.com>
2023-06-21 23:16:48 +0530

    Hashing Terminology: Hash Function, Key, Hash Value, Hash Table, Collision

---
+++ 23 - Hashing/23.02 Hashing Terminology.py
@@ -0,0 +1,13 @@
+class Hashing:
+    '''
+    Hashing Terminology
+      Hash function - It is a function that can be used to map an arbitrary size to data of fixed size
+      Key: Input data by a user
+      Hash value - A value that is returned by the hashing function
+      Hash table - It is a data structure which implements an associative array abstract data type, a structure that can map keys and values
+      Collision - A collision occurs when two different keys to a hashfunction produces same output
+    '''
+
+
+if __name__ == "__main__":
+    print(Hashing().__doc__)
