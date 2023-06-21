7a5e1bbeb293e8e5142f4d3a0cb91878592838fa
TarakaKoda <taraka.koda@gmail.com>
2023-06-21 23:56:41 +0530

    Practical use of Hashing

---
+++ 23 - Hashing/23.07 Practical use of Hashing.py
@@ -0,0 +1,13 @@
+class Hashing:
+    '''
+    Practical use of Hashing
+      - password verification
+      - File system 0 File path is mapped to physical location on disk
+
+    Pros and cons of hashing
+      - On average insertion/Deletion/Search operations take O(1) time.
+      - when hash function is not good enough insertion/deletion/search operation take O(n) time
+    '''
+
+if __name__ == "__main__":
+    print(Hashing().__doc__)
