6d7b6609fef9dd66279b1fc7dce82986c2d70dbb
TarakaKoda <taraka.koda@gmail.com>
2023-06-22 23:18:34 +0530

    Sorting Terminology

---
+++ 24 - Sorting Algorithms/24.03 Sorting Terminology.py
@@ -0,0 +1,20 @@
+class Sorting:
+    '''
+    Sorting Terminology:
+
+        1. Increasing Order: If a Successive element is greater than the Previous element in a sequence.
+           Example: 1, 3, 5, 7, 9, 11.
+
+        2. Decreasing Order: If a Successive element is less than the Previous Element in a sequence.
+           Example: 11, 9, 7, 5, 3 , 1.
+
+        3. Non Increasing Order: If a Successive element is less than or equal to the Previous element in a sequence.
+           Example: 11, 7, 5, 5, 3, 1.
+
+        4. Non Decreasing Order: If a Successive element is greater than or equal to the Previous element in a sequence.
+           Example: 1, 3, 5, 7, 7, 9 , 11.
+
+    '''
+
+if __name__ == "__main__":
+    print(Sorting().__doc__)
