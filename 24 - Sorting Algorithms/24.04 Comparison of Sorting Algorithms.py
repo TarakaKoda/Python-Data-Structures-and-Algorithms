522dd205c02195816643baa2f8919e9f3b1b4fe8
TarakaKoda <taraka.koda@gmail.com>
2023-06-22 23:44:35 +0530

    Comparison of Sorting Algorithms

---
+++ 24 - Sorting Algorithms/24.04 Comparison of Sorting Algorithms.py
@@ -0,0 +1,17 @@
+class Sorting:
+    '''
+    --------------------------------------------------------------------------
+       NAME          Time Complexity       Space Complexity       Stability
+    --------------------------------------------------------------------------
+    Bubble Sort           O(N^2)                  O(1)               YES
+    Selection Sort        O(N^2)                  O(1)               NO
+    Insertion Sort        O(N^2)                  O(1)               YES
+    Bucket Sort           O(logN)                 O(N)               YES
+    Merge Sort            O(logN)                 O(N)               YES
+    Quick Sort            O(logN)                 O(N)               NO
+    Heap Sort             O(logN)                 O(1)               NO
+    --------------------------------------------------------------------------
+    '''
+
+if __name__ == "__main__":
+    print(Sorting().__doc__)
