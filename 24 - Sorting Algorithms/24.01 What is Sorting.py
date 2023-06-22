42ff189ec9175565e7b7f45b50442cfc766d57f8
TarakaKoda <taraka.koda@gmail.com>
2023-06-22 23:17:54 +0530

    Introduction to Sorting

---
+++ 24 - Sorting Algorithms/24.01 What is Sorting.py
@@ -0,0 +1,22 @@
+class Sorting:
+    '''
+    Sorting Algorithms Techniques:
+    - Bubble Sort
+    - Selection Sort
+    - Insertion Sort
+    - Bucket Sort
+    - Merge Sort
+    - Quick Sort
+    - Heap Sort
+
+    What is Sorting?
+    - By definition Sorting refers to arranging data in a particular format: Either ascending or descending order
+
+    Practical use of Sorting:
+    1. Microsoft Excel : Built in functionality to sort data
+    2. Online Store: Online shopping websites generally  have option for sorting by Price, Reviews, Rating...
+    '''
+
+
+if __name__ == "__main__":
+    print(Sorting().__doc__)
