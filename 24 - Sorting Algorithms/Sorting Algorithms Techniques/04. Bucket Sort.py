35fec107c824e3760dd7a2a73d5bc8c6c35a7164
TarakaKoda <taraka.koda@gmail.com>
2023-06-23 23:17:21 +0530

    Updated Bucket Sort: Merge sort, quick sort added

--- 24 - Sorting Algorithms/Sorting Algorithms Techniques/04. Bucket Sort.py
+++ 24 - Sorting Algorithms/Sorting Algorithms Techniques/04. Bucket Sort.py
@@ -46,6 +46,49 @@ class Bucket:
                 array[j+1] = key
         return array
 
+    @staticmethod
+    def merge(left, right, reverse=False):
+        result = []
+        i, j = 0, 0
+        if not reverse:
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+        else:
+            while i < len(left) and j < len(right):
+                if left[i] >= right[j]:
+                    result.append(left[i])
+                    i += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+        result += left[i:]
+        result += right[j:]
+        return result
+
+    @staticmethod
+    def merge_sort(array):
+        if len(array) <= 1:
+            return array
+        mid = int(len(array))
+        left = Bucket.merge_sort(array[:mid])
+        right = Bucket.merge_sort(array[mid:])
+        return Bucket.merge(left, right)
+
+    @staticmethod
+    def quick_sort(array):
+        if len(array) <= 1:
+            return array
+        else:
+            pivot = array[len(array)//2]
+            left = [x for x in array if x < pivot]
+            middle = [x for x in array if x == pivot]
+            right = [x for x in array if x > pivot]
+            return Bucket.quick_sort(left) + middle + Bucket.quick_sort(right)
 
 
 if __name__ == '__main__':
