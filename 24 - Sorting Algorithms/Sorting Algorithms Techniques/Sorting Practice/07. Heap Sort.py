b400fc87baf5c45a7fcd9b778bf3ed3f08f8e9c5
TarakaKoda <taraka.koda@gmail.com>
2023-06-23 23:19:12 +0530

    Reconstructed and tested Heap Sort

--- 24 - Sorting Algorithms/Sorting Algorithms Techniques/Sorting Practice/07. Heap Sort.py
+++ 24 - Sorting Algorithms/Sorting Algorithms Techniques/Sorting Practice/07. Heap Sort.py
@@ -1,24 +1,30 @@
 
-def heapify(customList, n, i):
+def heapify(custom_list, n, i):
     smallest = i
     l = 2 * i + 1
     r = 2 * i + 2
-    if l < n and customList[l] < customList[smallest]:
+    if l < n and custom_list[l] < custom_list[smallest]:
         smallest = l
 
-    if r < n and customList[r] < customList[smallest]:
+    if r < n and custom_list[r] < custom_list[smallest]:
         smallest = r
 
     if smallest != i:
-        customList[i], customList[smallest] = customList[smallest], customList[i]
-        heapify(customList, n, smallest)
+        custom_list[i], custom_list[smallest] = custom_list[smallest], custom_list[i]
+        heapify(custom_list, n, smallest)
 
-def heapSort(customList):
-    n = len(customList)
+def heap_sort(custom_list):
+    n = len(custom_list)
     for i in range(int(n / 2) - 1, -1, -1):
-        heapify(customList, n, i)
+        heapify(custom_list, n, i)
 
     for i in range(n - 1, 0, -1):
-        customList[i], customList[0] = customList[0], customList[i]
-        heapify(customList, i, 0)
-    # customList.reverse()
+        custom_list[i], custom_list[0] = custom_list[0], custom_list[i]
+        heapify(custom_list, i, 0)
+    custom_list.reverse()
+
+numbers = [1,4,6,7,2,3,8,9]
+
+if __name__ == "__main__":
+    heap_sort(numbers)
+    print(numbers)
