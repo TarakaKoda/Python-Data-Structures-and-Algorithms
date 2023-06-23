f806838d4038f50259b4c825146a57bb193e3d75
TarakaKoda <taraka.koda@gmail.com>
2023-06-23 23:16:04 +0530

    Updated Bubble Sort

--- 24 - Sorting Algorithms/Sorting Algorithms Techniques/Sorting Practice/01. Bubble Sort.py
+++ 24 - Sorting Algorithms/Sorting Algorithms Techniques/Sorting Practice/01. Bubble Sort.py
@@ -2,7 +2,11 @@ class Bubble:
 
     @staticmethod
     def sort(array, reverse=False):
-        if not reverse:
+        if not array:
+            return None
+        if len(array) == 1:
+            return array
+        elif not reverse:
             for i in range(len(array)):
                 for j in range(len(array)-i-1):
                     if array[j] > array[j + 1]:
@@ -17,44 +21,48 @@ class Bubble:
 
     @staticmethod
     def optimized(array, reverse=False):
-        if reverse:
-            swap = False
-            for i in range(len(array) - 1):
-                for j in range(len(array) - i - 1):
-                    if array[j] < array[j + 1]:
-                        array[j], array[j + 1] = array[j + 1], array[j]
-                        swap = True
-                if not swap:
-                    break
-        else:
-            swap = False
-            for i in range(len(array) - 1):
-                for j in range(len(array) - i - 1):
+        if array is None:
+            return None
+        if len(array) == 1:
+            return array
+        swap = False
+        if reverse is False:
+            for i in range(len(array)):
+                for j in range(len(array)-i-1):
                     if array[j] > array[j + 1]:
                         array[j], array[j + 1] = array[j + 1], array[j]
                         swap = True
                 if not swap:
                     break
+        else:
+            for i in range(len(array)):
+                for j in range(len(array)-i-1):
+                    if array[j] < array[j+1]:
+                        array[j], array[j+1] = array[j+1], array[j]
+                        swap = True
+                if not swap:
+                    break
         return array
-
     @staticmethod
     def recursive(array, n=None):
-        if not n:
+        if n == None:
             n = len(array)
-        if n == 1: return
+        if n == 1:
+            return array
         swap = False
         for i in range(n-1):
             if array[i] > array[i + 1]:
-                array[i], array[i + 1] = array[i + 1], array[i]
+                array[i], array[i + 1] =  array[i + 1], array[i]
                 swap = True
-        if not swap:
+        if swap is None:
             return
         Bubble.recursive(array, n-1)
 
 
-numbers = [2,3,4,5,6,1,7,8]
+numbers = [9,5,6,4,3,2,8,1,7]
 
 if __name__ == "__main__":
-    print(Bubble.sort(numbers, False))
+    print(Bubble.sort(numbers, True))
+    print(Bubble.optimized(numbers, True))
     Bubble.recursive(numbers)
     print(numbers)
