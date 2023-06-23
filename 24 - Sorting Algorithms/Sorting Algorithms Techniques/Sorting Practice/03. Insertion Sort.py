8335052c5666a4c41e7409ced3034fc3ddcafc07
TarakaKoda <taraka.koda@gmail.com>
2023-06-23 23:16:44 +0530

    Updated Insertion Sort

--- 24 - Sorting Algorithms/Sorting Algorithms Techniques/Sorting Practice/03. Insertion Sort.py
+++ 24 - Sorting Algorithms/Sorting Algorithms Techniques/Sorting Practice/03. Insertion Sort.py
@@ -1,26 +1,26 @@
 class Insertion:
-
     @staticmethod
-    def Sort(array, reverse=False):
-        if not reverse:
+    def sort(array, reverse=False):
+        if len(array) == 1:
+            return array
+        elif not reverse:
             for i in range(1, len(array)):
                 key = array[i]
-                j = i - 1
-                while j>= 0 and key < array[j]:
+                j = i-1
+                while j>=0 and key < array[j]:
                     array[j+1] = array[j]
                     j -= 1
                 array[j+1] = key
         else:
             for i in range(1, len(array)):
                 key = array[i]
-                j = i - 1
-                while j>= 0 and key > array[j]:
+                j = i -1
+                while j>=0 and key > array[j]:
                     array[j+1] = array[j]
-                    j -= 1
+                    j -=1
                 array[j+1] = key
         return array
-
-numbers = [3,5,6,2,1,8,7,4]
+numbers = [2,5,1,3,6,4,7,9,8]
 
 if __name__ == "__main__":
-    print(Insertion.Sort(numbers, True))
+    print(Insertion.sort(numbers, True))
