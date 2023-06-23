b3a71da236f4e6ca6db2aabfffedd489df5384c4
TarakaKoda <taraka.koda@gmail.com>
2023-06-23 23:18:35 +0530

    Standard Quick Sort Added

--- 24 - Sorting Algorithms/Sorting Algorithms Techniques/Sorting Practice/06. Quick Sort.py
+++ 24 - Sorting Algorithms/Sorting Algorithms Techniques/Sorting Practice/06. Quick Sort.py
@@ -15,3 +15,20 @@ class Quick:
             partition = self.partition(values, start, end)
             self.sort(values, start, partition - 1)
             self.sort(values, partition + 1, end)
+
+
+    @staticmethod
+    def quick_sort(array):
+        if len(array) <= 1:
+            return array
+        else:
+            pivot = array[len(array)//2]
+            left = [x for x in array if x < pivot]
+            middle = [x for x in array if x == pivot]
+            right = [x for x in array if x > pivot]
+            return Quick.quick_sort(left) + middle + Quick.quick_sort(right)
+
+numbers = [2,-1,0,1,4,6,3,7,9,8]
+
+if __name__ == "__main__":
+    print(Quick.quick_sort(numbers))
