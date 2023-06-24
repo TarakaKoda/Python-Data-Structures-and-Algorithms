9312cc0412d718c217b645ec9980cb1261b48012
TarakaKoda <taraka.koda@gmail.com>
2023-06-24 21:31:59 +0530

    Introduction to Searching Algorithm

---
+++ 25 - Searching Algorithms/01. Introduction to Searching Algorithms.py
@@ -0,0 +1,24 @@
+class Searching:
+    '''
+     Searching algorithms are an essential topic in computer science and software development. Here's a brief overview
+     of some commonly used searching algorithms:
+
+    Linear Search: In a linear search, each element in a list or array is checked sequentially until a match is found or
+                   the entire list is traversed.
+
+    Binary Search: Binary search is a more efficient algorithm for searching sorted arrays. It repeatedly divides the
+                   search space in half until the target element is found.
+
+    Hashing: Hashing is a technique that uses a hash function to map data to a fixed-size array called a hash table. It
+             provides constant-time average case search complexity.
+
+    Depth-First Search (DFS): DFS is a graph traversal algorithm that explores as far as possible along each branch
+                              before backtracking. It can be used for searching in graph-based data structures.
+
+    Breadth-First Search (BFS): BFS is another graph traversal algorithm that explores all the vertices of a graph in
+                                breadth-first order, starting from a given source vertex.
+    '''
+
+
+if __name__ == "__main__":
+    print(Searching().__doc__)
