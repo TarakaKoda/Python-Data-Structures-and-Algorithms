81dbb462c73eafec133163856b740d83c5dd7c4a
TarakaKoda <taraka.koda@gmail.com>
2023-06-24 21:38:53 +0530

    Putting learning into practice: A comprehensive exercises

--- Practice.py
+++ Practice.py
@@ -2783,12 +2783,37 @@ class Selection:
                 array[i],  array[minimum_value], = array[minimum_value], array[i]
         return array
 
-numbers = [2,3,1,4,6,5,7,9,8]
+# numbers = [2,3,1,4,6,5,7,9,8]
+#
+# if __name__ == "__main__":
+#     print(Selection.sort(numbers, True))
+
+class Insertion:
+    @staticmethod
+    def sort(array, reverse=False):
+        if len(array) == 1:
+            return array
+        elif not reverse:
+            for i in range(1, len(array)):
+                key = array[i]
+                j = i-1
+                while j>=0 and key < array[j]:
+                    array[j+1] = array[j]
+                    j -= 1
+                array[j+1] = key
+        else:
+            for i in range(1, len(array)):
+                key = array[i]
+                j = i -1
+                while j>=0 and key > array[j]:
+                    array[j+1] = array[j]
+                    j -=1
+                array[j+1] = key
+        return array
+numbers = [2,5,1,3,6,4,7,9,8]
 
 if __name__ == "__main__":
-    print(Selection.sort(numbers, True))
-
-
+    print(Insertion.sort(numbers, True))
 
 
 
