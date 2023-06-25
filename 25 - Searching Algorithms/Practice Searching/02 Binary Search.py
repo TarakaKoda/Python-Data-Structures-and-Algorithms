12db1a08685175cb0e3bd056d1fc9a97b91fb9b4
TarakaKoda <taraka.koda@gmail.com>
2023-06-25 22:49:15 +0530

    Binary Search: O(logN)

---
+++ 25 - Searching Algorithms/Practice Searching/02 Binary Search.py
@@ -0,0 +1,33 @@
+class Binary_Search:
+    @staticmethod
+    def binary_search(arr, target):
+        low = 0
+        high = len(arr) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1  # Target value not found
+
+# Example usage
+my_list = [99, 109, 125, 296, 299, 310]
+target_value = 109
+
+result = Binary_Search.binary_search(my_list, target_value)
+
+if __name__ == "__main__":
+    # Example usage
+    my_list = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+    target_value = 12
+
+    if result != -1:
+        print(f"Target value {target_value} found at index {result}")
+    else:
+        print("Target value not found in the list.")
